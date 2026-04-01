84f9b7eccf2ce7ba4eca5db8ed79841f6ecb57b5
Tehnikus <tehnikus.ua@gmail.com>
2026-04-01 20:21:59 +0300

    Add Twig {% cache %} tag support

--- upload/system/library/template/twig.php
+++ upload/system/library/template/twig.php
@@ -1,41 +1,84 @@
 <?php
+
 namespace Template;
+
+use Twig\Extra\Cache\CacheExtension;
+use Twig\Extra\Cache\CacheRuntime;
+use Twig\RuntimeLoader\RuntimeLoaderInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
+
 final class Twig {
-	private $data = array();
 
-	public function set($key, $value) {
-		$this->data[$key] = $value;
-	}
-	
-	public function render($filename, $code = '') {
-		if (!$code) {
-			$file = DIR_TEMPLATE . $filename . '.twig';
+  private $data = [];
 
-			if (is_file($file)) {
-				$code = file_get_contents($file);
-			} else {
-				throw new \Exception('Error: Could not load template ' . $file . '!');
-				exit();
-			}
-		}
+  public function set($key, $value) {
+    $this->data[$key] = $value;
+  }
 
-		// initialize Twig environment
-		$config = array(
-			'autoescape'  => false,
-			'debug'       => false,
-			'auto_reload' => true,
-			'cache'       => DIR_CACHE . 'template/'
-		);
+  public function render($filename, $code = '') {
 
-		try {
-			$loader = new \Twig\Loader\ArrayLoader(array($filename . '.twig' => $code));
+    // Get template file
+    $file = DIR_TEMPLATE . $filename . '.twig';
 
-			$twig = new \Twig\Environment($loader, $config);
+    if (!is_file($file)) {
+      throw new \Exception('Error: Could not load template ' . $file . '!');
+    }
 
-			return $twig->render($filename . '.twig', $this->data);
-		} catch (\Exception $e) {
-			trigger_error('Error: Could not load template ' . $filename . '! ' . $e->getMessage());
-			exit();
-		}	
-	}	
+    // FilesystemLoader instead of ArrayLoader
+    $paths = array_unique(array_filter([
+      rtrim(DIR_TEMPLATE, '/'), // Current template
+      rtrim(DIR_SYSTEM . '../catalog/view/theme/default/template', '/'), // fallback default
+    ]));
+
+    $loader = new \Twig\Loader\FilesystemLoader($paths);
+
+    //
+    // Twig Environment
+    //
+    $twig = new \Twig\Environment($loader, [
+      'autoescape'  => false,
+      'debug'       => false,
+      'auto_reload' => true,
+      'cache'       => DIR_CACHE . 'template/',
+    ]);
+
+    
+    // Add {% cache %} tag support
+		// Static cache folder
+    $fragmentCacheDir = DIR_CACHE . 'html/';
+
+    $twig->addExtension(new CacheExtension());
+
+    $twig->addRuntimeLoader(
+      new class ($fragmentCacheDir) implements RuntimeLoaderInterface {
+        public function __construct(private string $cacheDir) {}
+
+        public function load(string $class): ?object {
+          if ($class === CacheRuntime::class) {
+            return new CacheRuntime(
+							// Tag support
+              new TagAwareAdapter(
+                new FilesystemAdapter(
+                  namespace: 'twig',
+                  defaultLifetime: 0,
+                  directory: $this->cacheDir
+                )
+              )
+            );
+          }
+          return null;
+        }
+      }
+    );
+
+    // Render
+    try {
+      $result = $twig->render($filename . '.twig', $this->data);
+      return $result;
+    } catch (\Exception $e) {
+      trigger_error('Error: Could not load template ' . $filename . '! ' . $e->getMessage());
+      exit();
+    }
+  }
 }
