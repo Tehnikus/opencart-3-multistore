1212159c31888fb108919f11fcd606c70db7c0d7
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 01:19:01 +0300

    Update addCategory() method to support multistore

--- upload/admin/model/catalog/category.php
+++ upload/admin/model/catalog/category.php
@@ -1,63 +1,175 @@
 <?php
 class ModelCatalogCategory extends Model {
 	public function addCategory($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "category SET parent_id = '" . (int)$data['parent_id'] . "', `top` = '" . (isset($data['top']) ? (int)$data['top'] : 0) . "', `column` = '" . (int)$data['column'] . "', sort_order = '" . (int)$data['sort_order'] . "', status = '" . (int)$data['status'] . "', date_modified = NOW(), date_added = NOW()");
 
-		$category_id = $this->db->getLastId();
-
-		if (isset($data['image'])) {
-			$this->db->query("UPDATE " . DB_PREFIX . "category SET image = '" . $this->db->escape($data['image']) . "' WHERE category_id = '" . (int)$category_id . "'");
-		}
-
-		foreach ($data['category_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "category_description SET category_id = '" . (int)$category_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', meta_title = '" . $this->db->escape($value['meta_title']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "'");
-		}
-
-		// MySQL Hierarchical Data Closure Table Pattern
-		$level = 0;
-
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$data['parent_id'] . "' ORDER BY `level` ASC");
-
-		foreach ($query->rows as $result) {
-			$this->db->query("INSERT INTO `" . DB_PREFIX . "category_path` SET `category_id` = '" . (int)$category_id . "', `path_id` = '" . (int)$result['path_id'] . "', `level` = '" . (int)$level . "'");
-
-			$level++;
-		}
-
-		$this->db->query("INSERT INTO `" . DB_PREFIX . "category_path` SET `category_id` = '" . (int)$category_id . "', `path_id` = '" . (int)$category_id . "', `level` = '" . (int)$level . "'");
-
-		if (isset($data['category_filter'])) {
-			foreach ($data['category_filter'] as $filter_id) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "category_filter SET category_id = '" . (int)$category_id . "', filter_id = '" . (int)$filter_id . "'");
-			}
-		}
-
-		if (isset($data['category_store'])) {
-			foreach ($data['category_store'] as $store_id) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "category_to_store SET category_id = '" . (int)$category_id . "', store_id = '" . (int)$store_id . "'");
-			}
-		}
+		$this->db->query("START TRANSACTION");
 		
-		if (isset($data['category_seo_url'])) {
-			foreach ($data['category_seo_url'] as $store_id => $language) {
-				foreach ($language as $language_id => $keyword) {
-					if (!empty($keyword)) {
-						$this->db->query("INSERT INTO " . DB_PREFIX . "seo_url SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'category_id=" . (int)$category_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+		try {
+
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "category 
+				SET 
+					`parent_id` 		= '" . (int)$data['parent_id'] . "', 
+					`top` 					= '" . (isset($data['top']) ? (int)$data['top'] : 0) . "', 
+					`column` 				= '" . (int)$data['column'] . "', 
+					`sort_order` 		= '" . (int)$data['sort_order'] . "', 
+					`status` 				= '" . (int)$data['status'] . "', 
+					`date_modified` = NOW(), 
+					`date_added` 		= NOW()
+				");
+	
+			$category_id = $this->db->getLastId();
+	
+			if (isset($data['image'])) {
+				$this->db->query("
+					UPDATE " . DB_PREFIX . "category 
+					SET 
+						image = '" . $this->db->escape($data['image']) . "' 
+					WHERE category_id = '" . (int) $category_id . "'
+				");
+			}
+	
+			foreach ($data['category_description'] as $language_id => $value) {
+				$this->db->query("
+					INSERT INTO " . DB_PREFIX . "category_description 
+					SET 
+						`category_id` 			= '" . (int) $category_id . "', 
+						`language_id` 			= '" . (int) $language_id . "', 
+						`store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+						`name` 							= '" . $this->db->escape($value['name']) . "', 
+						`description` 			= '" . $this->db->escape($value['description']) . "', 
+						`meta_title` 				= '" . $this->db->escape($value['meta_title']) . "', 
+						`meta_description` 	= '" . $this->db->escape($value['meta_description']) . "', 
+						`meta_keyword` 			= '" . $this->db->escape($value['meta_keyword']) . "'
+				");
+			}
+	
+			// Start category tree
+			// Current store
+			$level = 0;
+	
+			$query = $this->db->query("
+				SELECT 
+					* 
+				FROM `" . DB_PREFIX . "category_path` 
+				WHERE `category_id` = '" . (int) $data['parent_id'] . "' 
+					AND `store_id` 		= '" . (int) $this->session->data['store_id'] . "'
+				ORDER BY `level` ASC
+			");
+	
+			foreach ($query->rows as $result) {
+				$this->db->query("
+					INSERT INTO `" . DB_PREFIX . "category_path` 
+					SET 
+						`category_id` = '" . (int) $category_id . "', 
+						`store_id` 		= '" . (int) $this->session->data['store_id'] . "',
+						`path_id` 		= '" . (int) $result['path_id'] . "', 
+						`level` 			= '" . (int) $level . "'
+				");
+	
+				$level++;
+			}
+	
+			$this->db->query("
+				INSERT INTO `" . DB_PREFIX . "category_path` 
+				SET 
+					`category_id` = '" . (int) $category_id . "', 
+					`store_id` 		= '" . (int) $this->session->data['store_id'] . "',
+					`path_id` 		= '" . (int) $category_id . "', 
+					`level` 			= '" . (int) $level . "'
+			");
+
+			// Other associated stores
+			foreach ($data['category_store'] as $store_id) {
+				if (((int) $store_id) !== ((int) $this->session->data['store_id'])) {
+					$this->db->query("
+						INSERT INTO `" . DB_PREFIX . "category_path` 
+						SET 
+							`category_id` = '" . (int) $category_id . "', 
+							`store_id` 		= '" . (int) $store_id . "',
+							`path_id` 		= '" . (int) $category_id . "', 
+							`level` 			= '0'
+					");
+				}
+			}
+
+			// End category tree
+
+			if (isset($data['category_filter'])) {
+				foreach ($data['category_filter'] as $filter_id) {
+					$this->db->query("
+						INSERT INTO " . DB_PREFIX . "category_filter 
+						SET 
+							`category_id` = '" . (int) $category_id . "', 
+							`store_id` 		= '" . (int) $this->session->data['store_id'] . "',
+							`filter_id` 	= '" . (int) $filter_id . "'
+					");
+				}
+			}
+	
+			if (isset($data['category_store'])) {
+				foreach ($data['category_store'] as $store_id) {
+					// Write data for current store
+					if (((int) $store_id) === ((int) $this->session->data['store_id'])) {
+						$this->db->query("
+							INSERT INTO " . DB_PREFIX . "category_to_store 
+							SET 
+								`category_id` 	= '" . (int) $category_id . "', 
+								`store_id`    	= '" . (int) $store_id . "',
+								`parent_id` 		= '" . (int) $data['parent_id'] . "', 
+								`status` 				= '" . (int) $data['status'] . "',
+								`sort_order` 		= '" . (int) $data['sort_order'] . "', 
+								`top` 					= '" . (isset($data['top']) ? (int) $data['top'] : 0) . "', 
+								`column` 				= '" . (int) $data['column'] . "',
+								`image` 				= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "'
+						");
+					} else {
+						// Set association for other selected stores
+						$this->db->query("
+							INSERT INTO " . DB_PREFIX . "category_to_store 
+							SET 
+								`category_id` 	= '" . (int) $category_id . "', 
+								`store_id`    	= '" . (int) $store_id . "'
+						");
 					}
 				}
 			}
-		}
-		
-		// Set which layout to use with this category
-		if (isset($data['category_layout'])) {
-			foreach ($data['category_layout'] as $store_id => $layout_id) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "category_to_layout SET category_id = '" . (int)$category_id . "', store_id = '" . (int)$store_id . "', layout_id = '" . (int)$layout_id . "'");
+			
+			if (isset($data['category_seo_url'])) {
+				foreach ($data['category_seo_url'] as $store_id => $language) {
+					foreach ($language as $language_id => $keyword) {
+						if (!empty($keyword)) {
+							$this->db->query("INSERT INTO " . DB_PREFIX . "seo_url SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'category_id=" . (int)$category_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+						}
+					}
+				}
 			}
+			
+			// Set which layout to use with this category
+			if (isset($data['category_layout'])) {
+				foreach ($data['category_layout'] as $store_id => $layout_id) {
+					$this->db->query("INSERT INTO " . DB_PREFIX . "category_to_layout SET category_id = '" . (int)$category_id . "', store_id = '" . (int)$store_id . "', layout_id = '" . (int)$layout_id . "'");
+				}
+			}
+			
+			$this->db->query("COMMIT");
+
+			// Rebuild facet indexes
+			$this->load->model('catalog/facet');
+			$store_id = (int) $this->session->data['store_id'];
+			$this->model_catalog_facet->buildFacetNames(facet_value_id: $category_id, facet_type: 1, store_id: $store_id);
+			$this->model_catalog_facet->buildFacetIndex(facet_value_id: $category_id, facet_type: 1, store_id: $store_id);
+			
+			// Delete cache
+			// While new category itself does not has cache yet, this method also clears it's parent category cache to update child categories 
+			$this->deleteCache($category_id);
+
+			return $category_id;
+			
+		} catch (\Throwable $e) {
+			$this->db->query("ROLLBACK");
+			throw $e;
 		}
-
-		$this->cache->delete('category');
-
-		return $category_id;
 	}
 
 	public function editCategory($category_id, $data) {
