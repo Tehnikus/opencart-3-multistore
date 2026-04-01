68c56fe12fc283236842084a3adaf3bd533ab83a
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 01:32:09 +0300

    Add multilanguage title and description for frontend homepage

--- upload/catalog/controller/common/home.php
+++ upload/catalog/controller/common/home.php
@@ -1,9 +1,24 @@
 <?php
 class ControllerCommonHome extends Controller {
 	public function index() {
-		$this->document->setTitle($this->config->get('config_meta_title'));
-		$this->document->setDescription($this->config->get('config_meta_description'));
-		$this->document->setKeywords($this->config->get('config_meta_keyword'));
+		// Get current language id
+		$config_language_id = $this->config->get('config_language_id');
+
+		// Set multilang homepage meta title
+		$config_meta_title = $this->config->get('config_meta_title');
+		$lang_meta_title = (is_array($config_meta_title) && isset($config_meta_title[$config_language_id])) ? $config_meta_title[$config_language_id] : '';
+
+		// Set multilang homepage meta description
+		$config_meta_description = $this->config->get('config_meta_description');
+		$lang_meta_description = (is_array($config_meta_description) && isset($config_meta_description[$config_language_id])) ? $config_meta_description[$config_language_id] : '';
+		
+		// Set multilang homepage meta keywords
+		$config_meta_keywords = $this->config->get('config_meta_keyword');
+		$lang_meta_keywords = (is_array($config_meta_keywords) && isset($config_meta_keywords[$config_language_id])) ? $config_meta_keywords[$config_language_id] : '';
+		
+		$this->document->setTitle($lang_meta_title);
+		$this->document->setDescription($lang_meta_description);
+		$this->document->setKeywords($lang_meta_keywords);
 
 		if (isset($this->request->get['route'])) {
 			$this->document->addLink($this->config->get('config_url'), 'canonical');
