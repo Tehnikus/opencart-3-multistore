3cf1a3f35c56c5e5afa0b9329f3005b7c146c5ea
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 02:52:41 +0300

    Add keyword translation

---
+++ upload/admin/language/en-gb/seo/keyword.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']             = 'Keywords';
+$_['keywords_list']             = 'Keywords';
+$_['keyword_groups']            = 'Keyword groups';
+$_['input_add_keyword_group']   = "Add new group";
+$_['button_add_row']            = 'Add new keyword';
+$_['button_delete']             = 'Delete';
+$_['button_import']             = 'Import CSV';
+$_['button_save_all']           = 'Save all';
+$_['button_clear_filters']      = 'Clear filters';
+$_['button_find_duplicates']    = 'Find duplicates';
+$_['button_add_tag']            = 'Add to tags';
+$_['button_replace_keywords']   = 'Replace keywords';
+$_['button_copy']               = 'Copy keyword';
+$_['button_delete']             = 'Delete keyword';
+
+$_['option_all_stores']         = 'All stores';
+$_['option_all_languages']      = 'All languages';
+
+$_['option_all_types']          = 'All types';
+$_['option_existing']           = 'Existing';
+$_['option_new']                = 'New';
+$_['option_updated']            = 'Updated';
+$_['option_imported']           = 'Imported';
+
+$_['column_seo_keyword']        = 'Keyword';
+$_['column_add_keyword']        = 'Add';
+$_['column_edit_keyword']       = 'Edit';
+$_['column_url']                = 'URL';
+$_['column_language']           = 'Language';
+$_['column_store']              = 'Store';
+$_['column_group']              = 'Group';
+$_['text_search']               = 'Search';
