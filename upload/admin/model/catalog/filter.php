dbbca4b59242587d3b830f048d3bf3a108b42f1a
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 01:10:06 +0300

    Update getFilter() method to support multistore

--- upload/admin/model/catalog/filter.php
+++ upload/admin/model/catalog/filter.php
@@ -500,8 +500,24 @@ class ModelCatalogFilter extends Model {
 		return $filter_group_data;
 	}
 
+	// Get filter description for category and product forms
 	public function getFilter($filter_id) {
-		$query = $this->db->query("SELECT *, (SELECT name FROM " . DB_PREFIX . "filter_group_description fgd WHERE f.filter_group_id = fgd.filter_group_id AND fgd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM " . DB_PREFIX . "filter f LEFT JOIN " . DB_PREFIX . "filter_description fd ON (f.filter_id = fd.filter_id) WHERE f.filter_id = '" . (int)$filter_id . "' AND fd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("
+			SELECT 
+				*, 
+				(SELECT 
+						name 
+					FROM " . DB_PREFIX . "filter_group_description fgd 
+					WHERE f.filter_group_id = fgd.filter_group_id 
+						AND fgd.language_id 	= '" . (int)$this->config->get('config_language_id') . "'
+						AND fgd.store_id 			= '" . $this->session->data['store_id'] . "'
+				) AS `group` 
+			FROM " . DB_PREFIX . "filter f 
+			LEFT JOIN " . DB_PREFIX . "filter_description fd ON (f.filter_id = fd.filter_id AND f.store_id = fd.store_id) 
+			WHERE f.filter_id 		= '" . (int)$filter_id . "' 
+				AND fd.language_id 	= '" . (int)$this->config->get('config_language_id') . "'
+				AND fd.store_id 		= '" . $this->session->data['store_id'] . "'
+		");
 
 		return $query->row;
 	}
