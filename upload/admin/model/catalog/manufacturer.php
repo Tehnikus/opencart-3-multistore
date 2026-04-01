ecc9e8fffee972ac7667204f2cb28c3272c987b2
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 02:41:35 +0300

    Update addManufacturer(), add multilang description, name and multistore support

--- upload/admin/model/catalog/manufacturer.php
+++ upload/admin/model/catalog/manufacturer.php
@@ -1,64 +1,177 @@
 <?php
 class ModelCatalogManufacturer extends Model {
-	public function addManufacturer($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "manufacturer SET name = '" . $this->db->escape($data['name']) . "', sort_order = '" . (int)$data['sort_order'] . "'");
+	public function addManufacturer($data) : int {
 
-		$manufacturer_id = $this->db->getLastId();
+		$this->db->query("START TRANSACTION");
+		
+		try {
 
-		if (isset($data['image'])) {
-			$this->db->query("UPDATE " . DB_PREFIX . "manufacturer SET image = '" . $this->db->escape($data['image']) . "' WHERE manufacturer_id = '" . (int)$manufacturer_id . "'");
-		}
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "manufacturer 
+				SET 
+					`name` 				= '" . $this->db->escape($data['manufacturer_description'][(int) $this->config->get('config_language_id')]['name']) . "', 
+					`sort_order` 	= '" . (int) $data['sort_order'] . "',
+					`image` 			= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "'
+			");
 
-		if (isset($data['manufacturer_store'])) {
-			foreach ($data['manufacturer_store'] as $store_id) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "manufacturer_to_store SET manufacturer_id = '" . (int)$manufacturer_id . "', store_id = '" . (int)$store_id . "'");
+			$manufacturer_id = $this->db->getLastId();
+
+			if (isset($data['manufacturer_store'])) {
+				foreach ($data['manufacturer_store'] as $store_id) {
+					$this->db->query("
+						INSERT INTO " . DB_PREFIX . "manufacturer_to_store 
+						SET 
+							`manufacturer_id` = '" . (int) $manufacturer_id . "', 
+							`store_id` 				= '" . (int) $store_id . "',
+							`sort_order` 			= '" . (int) $data['sort_order'] . "',
+							`image` 					= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "'
+					");
+				}
 			}
-		}
-				
-		// SEO URL
-		if (isset($data['manufacturer_seo_url'])) {
-			foreach ($data['manufacturer_seo_url'] as $store_id => $language) {
-				foreach ($language as $language_id => $keyword) {
-					if (!empty($keyword)) {
-						$this->db->query("INSERT INTO " . DB_PREFIX . "seo_url SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'manufacturer_id=" . (int)$manufacturer_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+
+			foreach ($data['manufacturer_description'] as $language_id => $value) {
+				$this->db->query("
+					INSERT INTO " . DB_PREFIX . "manufacturer_description 
+					SET 
+						`manufacturer_id` 	= '" . (int) $manufacturer_id . "', 
+						`language_id` 			= '" . (int) $language_id . "', 
+						`store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+						`name` 							= '" . $this->db->escape($value['name']) . "', 
+						`description` 			= '" . $this->db->escape($value['description']) . "', 
+						`meta_title` 				= '" . $this->db->escape($value['meta_title']) . "', 
+						`meta_description` 	= '" . $this->db->escape($value['meta_description']) . "', 
+						`meta_keyword` 			= '" . $this->db->escape($value['meta_keyword']) . "'
+				");
+			}
+					
+			// SEO URL
+			if (isset($data['manufacturer_seo_url'])) {
+				foreach ($data['manufacturer_seo_url'] as $store_id => $language) {
+					foreach ($language as $language_id => $keyword) {
+						if (!empty($keyword)) {
+							$this->db->query("INSERT INTO " . DB_PREFIX . "seo_url SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'manufacturer_id=" . (int)$manufacturer_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+						}
 					}
 				}
 			}
-		}
-		
-		$this->cache->delete('manufacturer');
 
-		return $manufacturer_id;
+			$this->db->query("COMMIT");
+
+			// Rebuild facet indexes
+			$this->load->model('catalog/facet');
+			$store_id = (int) $this->session->data['store_id'];
+			$this->model_catalog_facet->buildFacetNames(facet_value_id: $manufacturer_id, facet_type: 5, store_id: $store_id);
+			$this->model_catalog_facet->buildFacetIndex(facet_value_id: $manufacturer_id, facet_type: 5, store_id: $store_id);
+			
+			return $manufacturer_id;
+		} catch (\Throwable $e) {
+			$this->db->query("ROLLBACK");
+			throw $e;
+		}
 	}
 
-	public function editManufacturer($manufacturer_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "manufacturer SET name = '" . $this->db->escape($data['name']) . "', sort_order = '" . (int)$data['sort_order'] . "' WHERE manufacturer_id = '" . (int)$manufacturer_id . "'");
+	public function editManufacturer($manufacturer_id, $data) : int {
 
-		if (isset($data['image'])) {
-			$this->db->query("UPDATE " . DB_PREFIX . "manufacturer SET image = '" . $this->db->escape($data['image']) . "' WHERE manufacturer_id = '" . (int)$manufacturer_id . "'");
-		}
+		$this->db->query("START TRANSACTION");
+		
+		try {
 
-		$this->db->query("DELETE FROM " . DB_PREFIX . "manufacturer_to_store WHERE manufacturer_id = '" . (int)$manufacturer_id . "'");
+			$this->db->query("
+				UPDATE " . DB_PREFIX . "manufacturer 
+				SET 
+					`name` 				= '" . $this->db->escape($data['name']) . "', 
+					`sort_order` 	= '" . (int) $data['sort_order'] . "',
+					`image` 			= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "'
+				WHERE manufacturer_id = '" . (int)$manufacturer_id . "'
+			");
 
-		if (isset($data['manufacturer_store'])) {
-			foreach ($data['manufacturer_store'] as $store_id) {
-				$this->db->query("INSERT INTO " . DB_PREFIX . "manufacturer_to_store SET manufacturer_id = '" . (int)$manufacturer_id . "', store_id = '" . (int)$store_id . "'");
+			$this->db->query("
+				DELETE FROM " . DB_PREFIX . "manufacturer_description 
+				WHERE `manufacturer_id` = '" . (int) $manufacturer_id . "'
+					AND `store_id` 				= '" . (int) $this->session->data['store_id'] . "'
+			");
+	
+			foreach ($data['manufacturer_description'] as $language_id => $value) {
+				$this->db->query("
+					INSERT INTO " . DB_PREFIX . "manufacturer_description 
+					SET 
+						`manufacturer_id` 	= '" . (int) $manufacturer_id . "', 
+						`language_id` 			= '" . (int) $language_id . "', 
+						`store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+						`name` 							= '" . $this->db->escape($value['name']) . "', 
+						`description` 			= '" . $this->db->escape($value['description']) . "',
+						`meta_title` 				= '" . $this->db->escape($value['meta_title']) . "', 
+						`meta_description` 	= '" . $this->db->escape($value['meta_description']) . "', 
+						`meta_keyword` 			= '" . $this->db->escape($value['meta_keyword']) . "'
+				");
 			}
-		}
 
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "seo_url` WHERE query = 'manufacturer_id=" . (int)$manufacturer_id . "'");
+			// Store related data
+			// Remove all unselected stores
+			$this->db->query("
+				DELETE FROM " . DB_PREFIX . "manufacturer_to_store
+				WHERE `manufacturer_id` = '" . (int) $manufacturer_id . "'
+					AND `store_id` NOT IN (" . implode(',', array_map('intval', $data['manufacturer_store'])) . ")
+			");
 
-		if (isset($data['manufacturer_seo_url'])) {
-			foreach ($data['manufacturer_seo_url'] as $store_id => $language) {
-				foreach ($language as $language_id => $keyword) {
-					if (!empty($keyword)) {
-						$this->db->query("INSERT INTO `" . DB_PREFIX . "seo_url` SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'manufacturer_id=" . (int)$manufacturer_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+			// Remove only current store no matter if it's selected
+			$this->db->query("
+				DELETE FROM " . DB_PREFIX . "manufacturer_to_store
+				WHERE `manufacturer_id` = '" . (int) $manufacturer_id . "'
+					AND `store_id` 		= '" . (int) $this->session->data['store_id'] . "'
+			");
+	
+			// Write store association and store related data
+			if (isset($data['manufacturer_store'])) {
+				foreach ($data['manufacturer_store'] as $store_id) {
+					if (((int) $store_id) === ((int) $this->session->data['store_id'])) {
+						// Set data for current store
+						$this->db->query("
+							INSERT INTO " . DB_PREFIX . "manufacturer_to_store 
+							SET 
+								`manufacturer_id` = '" . (int) $manufacturer_id . "', 
+								`store_id` 				= '" . (int) $store_id . "',
+								`sort_order` 			= '" . (int) $data['sort_order'] . "',
+								`image` 					= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "'
+						");
+					} else {
+						// Skip if data for other stores already exists
+						$this->db->query("
+							INSERT IGNORE INTO " . DB_PREFIX . "manufacturer_to_store
+							SET 
+								`manufacturer_id` = '" . (int) $manufacturer_id . "',
+								`store_id`    		= '" . (int) $store_id . "'
+						");
 					}
 				}
 			}
-		}
 
-		$this->cache->delete('manufacturer');
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "seo_url` WHERE query = 'manufacturer_id=" . (int)$manufacturer_id . "'");
+
+			if (isset($data['manufacturer_seo_url'])) {
+				foreach ($data['manufacturer_seo_url'] as $store_id => $language) {
+					foreach ($language as $language_id => $keyword) {
+						if (!empty($keyword)) {
+							$this->db->query("INSERT INTO `" . DB_PREFIX . "seo_url` SET store_id = '" . (int)$store_id . "', language_id = '" . (int)$language_id . "', query = 'manufacturer_id=" . (int)$manufacturer_id . "', keyword = '" . $this->db->escape($keyword) . "'");
+						}
+					}
+				}
+			}
+
+			$this->db->query("COMMIT");
+
+			// Rebuild facet indexes
+			$this->load->model('catalog/facet');
+			$store_id = (int) $this->session->data['store_id'];
+			$this->model_catalog_facet->buildFacetNames(facet_value_id: $manufacturer_id, facet_type: 5, store_id: $store_id);
+			$this->model_catalog_facet->buildFacetIndex(facet_value_id: $manufacturer_id, facet_type: 5, store_id: $store_id);
+			
+			return $manufacturer_id;
+			
+		} catch (\Throwable $e) {
+			$this->db->query("ROLLBACK");
+			throw $e;
+		}
 	}
 
 	public function deleteManufacturer($manufacturer_id) {
