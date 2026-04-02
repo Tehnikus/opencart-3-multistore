125c890e7e946c96d13ef18d20e9f0fcd7b96803
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 12:16:48 +0300

    Update frontend module model to support store_id context

--- upload/catalog/model/setting/module.php
+++ upload/catalog/model/setting/module.php
@@ -1,7 +1,13 @@
 <?php
 class ModelSettingModule extends Model {
 	public function getModule($module_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module WHERE module_id = '" . (int)$module_id . "'");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "module 
+			WHERE `module_id` = '" . (int) $module_id . "'
+				AND `store_id`  = '" . (int) $this->config->get('config_store_id') . "'
+		");
 		
 		if ($query->row) {
 			return json_decode($query->row['setting'], true);
