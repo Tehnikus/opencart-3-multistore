a33cbcd4135444dc933268b6ab76e4ec6e82feec
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 12:07:08 +0300

    Add store_id context to extensions in frontend

--- upload/catalog/model/setting/extension.php
+++ upload/catalog/model/setting/extension.php
@@ -1,7 +1,13 @@
 <?php
 class ModelSettingExtension extends Model {
 	public function getExtensions($type) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "extension WHERE `type` = '" . $this->db->escape($type) . "'");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "extension 
+			WHERE `type` 			= '" . $this->db->escape($type) . "'
+				AND `store_id` 	= '" . (int) $this->config->get('config_store_id') . "'	
+		");
 
 		return $query->rows;
 	}
