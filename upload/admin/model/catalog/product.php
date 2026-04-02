d71f218c964a10e8ba1d561783c8e5e148e99f6c
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 03:29:29 +0300

    Update getProductsByCategoryId() method (not used?)

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -1255,8 +1255,21 @@ class ModelCatalogProduct extends Model {
 		return $result;
 
 	}
+
+	// Not used?
 	public function getProductsByCategoryId($category_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id) WHERE pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p2c.category_id = '" . (int)$category_id . "' ORDER BY pd.name ASC");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "product p 
+			LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.`product_id` = pd.`product_id`) 
+			LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.`product_id` = p2c.`product_id`) 
+			WHERE pd.`language_id`  = '" . (int) $this->config->get('config_language_id') . "' 
+				AND p2c.`category_id` = '" . (int) $category_id . "' 
+				AND pd.`store_id` 		= '" . (int) $this->session->data['store_id'] . "'
+				AND p2c.`store_id` 		= '" . (int) $this->session->data['store_id'] . "'
+			ORDER BY pd.`name` ASC
+		");
 
 		return $query->rows;
 	}
