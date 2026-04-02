ca43a7b504b3fae4f5fd26f3d91d52e63e27d536
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 12:21:27 +0300

    Add getStoresAssociation() method

--- upload/admin/model/catalog/option.php
+++ upload/admin/model/catalog/option.php
@@ -550,4 +550,25 @@ class ModelCatalogOption extends Model {
 
 		return $query->row['total'];
 	}
+
+	public function getStoresAssociation($id = null) : array {
+		$result = [];
+
+		if (!$id) {
+			return $result;
+		}
+
+		// Get stores association
+		$storeData = $this->db->query("
+			SELECT
+				store_id
+			FROM `" . DB_PREFIX . "option_to_store`
+			WHERE option_id = '" . (int) $id . "'
+		");
+		foreach ($storeData->rows as $store) {
+			$result[] = $store['store_id']; 
+		}
+
+		return $result;
+	}
 }
