5d992c9a8389c6479fa853bf39465d9b376eee0a
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 03:31:09 +0300

    Some more fixes in getProductAttributes() method

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -1423,18 +1423,25 @@ class ModelCatalogProduct extends Model {
 		");
 
 		foreach ($product_attribute_query->rows as $product_attribute) {
-			$product_attribute_description_data = array();
+			$product_attribute_description_data = [];
 
-			$product_attribute_description_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_attribute WHERE product_id = '" . (int)$product_id . "' AND attribute_id = '" . (int)$product_attribute['attribute_id'] . "'");
+			$product_attribute_description_query = $this->db->query("
+				SELECT 
+					* 
+				FROM " . DB_PREFIX . "product_attribute 
+				WHERE `product_id` 		= '" . (int) $product_id . "' 
+					AND `attribute_id` 	= '" . (int) $product_attribute['attribute_id'] . "'
+					AND `store_id` 			= '" . (int) $this->session->data['store_id'] . "'
+			");
 
 			foreach ($product_attribute_description_query->rows as $product_attribute_description) {
-				$product_attribute_description_data[$product_attribute_description['language_id']] = array('text' => $product_attribute_description['text']);
+				$product_attribute_description_data[$product_attribute_description['language_id']] = ['text' => $product_attribute_description['text']];
 			}
 
-			$product_attribute_data[] = array(
+			$product_attribute_data[] = [
 				'attribute_id'                  => $product_attribute['attribute_id'],
 				'product_attribute_description' => $product_attribute_description_data
-			);
+			];
 		}
 
 		return $product_attribute_data;
