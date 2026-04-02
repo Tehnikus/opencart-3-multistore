148196e5fbc9f2bbd231e608666631408446ef63
Tehnikus <tehnikus.ua@gmail.com>
2026-04-02 03:03:41 +0300

    Load languages in frontend in accordance with language to store association settings

--- upload/catalog/model/localisation/language.php
+++ upload/catalog/model/localisation/language.php
@@ -1,33 +1,45 @@
 <?php
 class ModelLocalisationLanguage extends Model {
 	public function getLanguage($language_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "language l
+			INNER JOIN " . DB_PREFIX . "language_to_store l2s
+				ON l.`language_id` = l2s.language_id
+				AND l2s.`store_id` = '" . (int) $this->config->get('config_store_id') . "'
+			WHERE l.`language_id` = '" . (int)$language_id . "'
+		");
 
 		return $query->row;
 	}
 
 	public function getLanguages() {
-		$language_data = $this->cache->get('catalog.language');
 
-		if (!$language_data) {
-			$language_data = array();
+		$language_data = [];
 
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE status = '1' ORDER BY sort_order, name");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "language l
+			INNER JOIN " . DB_PREFIX . "language_to_store l2s
+				ON l.`language_id` = l2s.`language_id`
+				AND l2s.`store_id` = '" . (int) $this->config->get('config_store_id') . "'
+			WHERE l.`status` = '1' 
+			ORDER BY `sort_order`, `name`
+		");
 
-			foreach ($query->rows as $result) {
-				$language_data[$result['code']] = array(
-					'language_id' => $result['language_id'],
-					'name'        => $result['name'],
-					'code'        => $result['code'],
-					'locale'      => $result['locale'],
-					'image'       => $result['image'],
-					'directory'   => $result['directory'],
-					'sort_order'  => $result['sort_order'],
-					'status'      => $result['status']
-				);
-			}
-
-			$this->cache->set('catalog.language', $language_data);
+		foreach ($query->rows as $result) {
+			$language_data[$result['code']] = array(
+				'language_id' => $result['language_id'],
+				'name'        => $result['name'],
+				'code'        => $result['code'],
+				'locale'      => $result['locale'],
+				'image'       => $result['image'],
+				'directory'   => $result['directory'],
+				'sort_order'  => $result['sort_order'],
+				'status'      => $result['status']
+			);
 		}
 
 		return $language_data;
