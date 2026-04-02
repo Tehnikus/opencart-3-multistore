b638db9da88c074e6ef043cb9e301ad724612b70
Tehnikus <tehnikus.ua@gmail.com>
2026-04-03 00:28:26 +0300

    Update translation

--- upload/admin/language/en-gb/seo/filter_page.php
+++ upload/admin/language/en-gb/seo/filter_page.php
@@ -11,10 +11,13 @@ $_['button_delete']             = 'Delete';
 $_['column_name']               = 'Name';
 
 // Errors
-$_['error_name']                = 'Set filter page name';
-$_['error_unique']              = 'Filter page SEO URL is not unique';
-$_['error_permission']          = 'You are not permitted to edit filter page';
-$_['error_warning']             = 'Error while saving filter page';
+$_['e_name']                    = 'Set filter page name';
+$_['e_unique']                  = 'Filter page SEO URL is not unique';
+$_['e_permission']              = 'You are not permitted to edit filter page';
+$_['e_warning']                 = 'Error while saving filter page';
+$_['e_select_category']         = 'Select parent category';
+$_['e_select_one_facet']        = 'Select at least one facet';
+$_['e_facet_not_unique']        = 'This facet combination already exists';
 
 // Messages
 $_['text_success']              = 'Filter page saved';
@@ -27,3 +30,13 @@ $_['button_save']               = 'Save';
 // Tabs
 $_['tab_general']               = 'General';
 $_['tab_description']           = 'Description';
+
+// Autocomplete
+$_['autocomplete_filter_page_category']     = 'Parent category';
+$_['autocomplete_filter_page_filter']       = 'Filters';
+$_['autocomplete_filter_page_option']       = 'Options';
+$_['autocomplete_filter_page_attribute']    = 'Attributes';
+$_['autocomplete_filter_page_manufacturer'] = 'Manufacturer';
+$_['autocomplete_filter_page_has_discount'] = 'Has discount';
+$_['autocomplete_filter_page_is_featured']  = 'Is featured';
+
