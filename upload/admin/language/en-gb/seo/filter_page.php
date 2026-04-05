89b4bb6944384569ffebb282d8f207460e107881
Tehnikus <tehnikus.ua@gmail.com>
2026-04-05 22:38:31 +0300

    Update translations

--- upload/admin/language/en-gb/seo/filter_page.php
+++ upload/admin/language/en-gb/seo/filter_page.php
@@ -22,8 +22,8 @@ $_['e_select_one_facet']        = 'Select at least one facet';
 $_['e_facet_not_unique']        = 'This facet combination already exists';
 
 // Messages
-$_['text_success']              = 'Filter page saved';
-$_['text_success_deleted']      = 'Filter page deleted';
+$_['text_filter_page_saved']    = 'Filter page saved';
+$_['text_filter_page_deleted']  = 'Filter page deleted';
 
 // Buttons
 $_['button_cancel']             = 'Cancel';
@@ -34,14 +34,14 @@ $_['tab_general']               = 'General';
 $_['tab_description']           = 'Description';
 
 // Autocomplete
-$_['filter_category_id']      = 'Category';
-$_['filter_filter']           = 'Filter';
-$_['filter_option']           = 'Option';
-$_['filter_attribute']        = 'Attribute';
-$_['filter_manufacturer_id']  = 'Manufacturer';
-$_['filter_tag_id']           = 'Tag';
-$_['filter_supplier_id']      = 'Supplier';
-$_['filter_is_available']     = 'Is available';
-$_['filter_has_discount']     = 'Has discount';
-$_['filter_is_featured']      = 'Is featured';
+$_['filter_category_id']        = 'Category';
+$_['filter_filter']             = 'Filter';
+$_['filter_option']             = 'Option';
+$_['filter_attribute']          = 'Attribute';
+$_['filter_manufacturer_id']    = 'Manufacturer';
+$_['filter_tag_id']             = 'Tag';
+$_['filter_supplier_id']        = 'Supplier';
+$_['filter_is_available']       = 'Is available';
+$_['filter_has_discount']       = 'Has discount';
+$_['filter_is_featured']        = 'Is featured';
 
