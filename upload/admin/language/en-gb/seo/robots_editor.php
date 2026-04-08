2baec7c7533b626f43326d12705a9e019cc5b3f7
Tehnikus <tehnikus.ua@gmail.com>
2026-04-08 14:06:13 +0300

    Add SeoRobotsEditor translation

---
+++ upload/admin/language/en-gb/seo/robots_editor.php
@@ -0,0 +1,5 @@
+<?php
+$_['heading_title'] = 'Edit robots.txt';
+$_['text_editor']   = 'Editor';
+$_['message_robots_saved']        = 'The robots.txt file saved successfully';
+$_['message_robots_not_writable'] = 'The robots.txt file is not writable. Seems like you need to set CHMOD 0755 or 0777 to your site root directory.';
