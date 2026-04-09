f28b97773ecd544355b15a4134817ae1af9c6d61
Tehnikus <tehnikus.ua@gmail.com>
2026-04-09 11:42:28 +0300

    Update Blog tags model

--- upload/admin/model/blog/tag.php
+++ upload/admin/model/blog/tag.php
@@ -1,51 +1,328 @@
 <?php
-class ModelBlogCategory extends Model {
+class ModelBlogTag extends Model {
+
+  public function addTag($data) {
+
+    $this->db->query("START TRANSACTION");
+
+    try {
+
+      // Save main table
+      $this->db->query("
+        INSERT INTO " . DB_PREFIX . "blog_tag_to_store 
+        SET
+          `store_id` 			 = '" . (int) $this->session->data['store_id'] . "', 
+          `date_added`     = NOW(),
+          `date_modified`  = NOW()
+        ");
+
+      $blog_tag_id = $this->db->getLastId();
+
+      // Save descriptions
+      foreach ($data['blog_tag_description'] as $language_id => $value) {
+        $this->db->query("
+          INSERT INTO " . DB_PREFIX . "blog_tag_description 
+          SET 
+            `blog_tag_id` 		        = '" . (int) $blog_tag_id . "', 
+            `language_id` 			= '" . (int) $language_id . "', 
+            `store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+            `name` 							= '" . $this->db->escape($value['name']) . "', 
+            `h1`                = '" . $this->db->escape($value['h1']) . "', 
+            `description` 			= '" . $this->db->escape($value['description']) . "', 
+            `meta_title` 				= '" . $this->db->escape($value['meta_title']) . "', 
+            `meta_description` 	= '" . $this->db->escape($value['meta_description']) . "', 
+            `meta_keyword` 			= '" . $this->db->escape($value['meta_keyword']) . "',
+            `seo_description` 	= '" . $this->db->escape($value['seo_description']) . "', 
+            `footer`            = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['footer'] ?? []), JSON_UNESCAPED_UNICODE)) . "',
+            `faq`               = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['faq'] ?? [], ['@type', '@context']), JSON_UNESCAPED_UNICODE)) . "',
+            `how_to`            = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['how_to'] ?? [], ['@type', '@context']), JSON_UNESCAPED_UNICODE)) . "'
+        ");
+      }
+
+      $this->editImages($blog_tag_id, $data['tag_images']);
+
+      // Save URL
+      foreach ($data['seo_url'] ?? [] as $language_id => $keyword) {
+        // Mostly safety delete, should never happen
+        $this->db->query("
+          DELETE FROM " . DB_PREFIX . "seo_url 
+          WHERE query       = 'blog_tag_id=" . $blog_tag_id . "'
+            AND language_id = '" . (int) $language_id . "'
+            AND store_id    = '" . (int) $this->session->data['store_id'] . "'
+        ");
+
+        if (!empty($keyword)) {
+          $this->db->query("
+            INSERT INTO " . DB_PREFIX . "seo_url 
+            SET 
+              store_id    = '" . (int) $this->session->data['store_id'] . "',
+              language_id = '" . (int) $language_id . "', 
+              query       = 'blog_tag_id=" . $blog_tag_id . "', 
+              keyword     = '" . $this->db->escape($keyword) . "'
+          ");
+        }
+      }
+      
+      $this->db->query("COMMIT");
+      return $blog_tag_id;
+      
+    } catch (\Throwable $e) {
+      $this->db->query("ROLLBACK");
+      throw $e;
+    }
+  }
+
+  public function editTag($blog_tag_id, $data) {
+    
+    $this->db->query("START TRANSACTION");
+
+    try {
+
+      // Update main table
+      $this->db->query("
+        UPDATE " . DB_PREFIX . "blog_tag_to_store 
+        SET
+          `date_modified`  = NOW()
+        WHERE blog_tag_id = " . (int) $blog_tag_id . "
+      ");
+
+      // Update descriptions
+      $this->db->query("
+        DELETE FROM " . DB_PREFIX . "blog_tag_description
+        WHERE blog_tag_id = " . (int) $blog_tag_id . "
+      ");
+
+      foreach ($data['blog_tag_description'] as $language_id => $value) {
+        $this->db->query("
+          INSERT INTO " . DB_PREFIX . "blog_tag_description 
+          SET 
+            `blog_tag_id` 		= '" . (int) $blog_tag_id . "', 
+            `language_id` 			= '" . (int) $language_id . "', 
+            `store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+            `name` 							= '" . $this->db->escape($value['name']) . "', 
+            `h1`                = '" . $this->db->escape($value['h1']) . "', 
+            `description` 			= '" . $this->db->escape($value['description']) . "', 
+            `meta_title` 				= '" . $this->db->escape($value['meta_title']) . "', 
+            `meta_description` 	= '" . $this->db->escape($value['meta_description']) . "', 
+            `meta_keyword` 			= '" . $this->db->escape($value['meta_keyword']) . "',
+            `seo_description` 	= '" . $this->db->escape($value['seo_description']) . "', 
+            `footer`            = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['footer'] ?? []), JSON_UNESCAPED_UNICODE)) . "',
+            `faq`               = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['faq'] ?? [], ['@type', '@context']), JSON_UNESCAPED_UNICODE)) . "',
+            `how_to`            = '" . $this->db->escape(json_encode($this->filterArrayRecursively($value['how_to'] ?? [], ['@type', '@context']), JSON_UNESCAPED_UNICODE)) . "'
+        ");
+      }
+
+      $this->editImages($blog_tag_id, $data['tag_images']);
+
+
+      // Save URL
+      foreach ($data['seo_url'] ?? [] as $language_id => $keyword) {
+        // Mostly safety delete, should never happen
+        $this->db->query("
+          DELETE FROM " . DB_PREFIX . "seo_url 
+          WHERE query       = 'blog_tag_id=" . $blog_tag_id . "'
+            AND language_id = '" . (int) $language_id . "'
+            AND store_id    = '" . (int) $this->session->data['store_id'] . "'
+        ");
+
+        if (!empty($keyword)) {
+          $this->db->query("
+            INSERT INTO " . DB_PREFIX . "seo_url 
+            SET 
+              store_id    = '" . (int) $this->session->data['store_id'] . "',
+              language_id = '" . (int) $language_id . "', 
+              query       = 'blog_tag_id=" . $blog_tag_id . "', 
+              keyword     = '" . $this->db->escape($keyword) . "'
+          ");
+        }
+      }
+      
+      $this->db->query("COMMIT");
+      return $blog_tag_id;
+      
+    } catch (\Throwable $e) {
+      $this->db->query("ROLLBACK");
+      throw $e;
+    }
+  }
+
+  public function editImages($page_id, $image_data = []) : int {
+
+    $page_id = (int) $page_id;
+    $store_id = (int) $this->session->data['store_id'];
+
+    $this->db->query("
+      DELETE FROM `". DB_PREFIX . "blog_tag_image` 
+      WHERE `blog_tag_id`    = '" . (int) $page_id . "'
+        AND store_id        = " . (int) $store_id . "
+    ");
+
+    $this->db->query("
+      DELETE FROM `". DB_PREFIX . "blog_tag_image_description` 
+      WHERE `blog_tag_id`    = '" . (int) $page_id . "'
+        AND store_id        = " . (int) $store_id . "
+    ");
+
+
+    foreach ($image_data as $image) {
+
+      // Check if image actually exists
+      if (!empty($image['image'])) {
+        $this->db->query("
+          INSERT INTO `". DB_PREFIX . "blog_tag_image`
+          SET 
+            `blog_tag_id` 	      = '" . (int) $page_id . "', 
+            `image` 				    = '" . $this->db->escape($image['image']) . "', 
+            `sort_order` 		    = '" . (int) $image['sort_order'] . "',
+            `store_id`          = '" . $store_id . "'
+        ");
+
+        // Add multilang multistore image descriptions
+        $image_id = $this->db->getLastId();
+
+        foreach ($image['description'] as $language_id => $image_description) {
+          if (!$image_description) {
+            continue;
+          }
+          $this->db->query("
+            INSERT INTO `". DB_PREFIX . "blog_tag_image_description`
+            SET
+              `image_id` 	      = '" . (int) $image_id . "',
+              `blog_tag_id` 	    = '" . (int) $page_id . "',
+              `language_id` 		= '" . (int) $language_id . "',
+              `store_id` 				= '" . (int) $store_id . "',
+              `description` 		= '" . $this->db->escape($image_description) ."'
+          ");
+        }
+      }
+    }
+
+    return $this->db->countAffected();
+  }
+
   public function getList($filter) : array {
-    $result = [];
-    $storeId = (int) $this->session->data['store_id'];
+    $result     = [];
+    $storeId    = (int) $this->session->data['store_id'];
+    $languageId = (int) $this->config->get('config_language_id');
+    
+    // Orders
+    $ordering = '';
+    $ordering = "ORDER BY " . ($this->sortOrders[$filter['sort']] ?? 'b2s.`date_modified`');
+    if (!empty($filter['order']) && in_array($filter['order'], ['ASC', 'DESC'])) {
+      $ordering .= " " . $filter['order'];
+    }
+      
+    // Limits
+    $limit  = max(1, (int) ($filter['limit'] ?? $this->config->get('config_limit_admin') ?? 20));
+    $start  = max(0, (int) ($filter['start'] ?? 0));
+    $limits = " LIMIT {$start}, {$limit}";
 
     $sql = "
       SELECT
-        *
-      FROM " . DB_PREFIX . "blog_category_description bcd
-      JOIN " . DB_PREFIX . "blog_category_to_store bc2s
-        ON bc2s.category_id = bcd.category_id
-        AND bc2s.store_id = {$storeId}
+        bd.`blog_tag_id`,
+        bd.`name`,
+        b2s.`date_modified`,
+        (SELECT bi.image FROM " . DB_PREFIX . "blog_tag_image bi WHERE bi.`blog_tag_id` = b2s.`blog_tag_id` AND bi.`store_id` = b2s.`store_id` ORDER BY bi.`sort_order` LIMIT 1) AS `image`,
+        (
+          SELECT JSON_OBJECT(
+            'descriptionLength',    COALESCE(CHAR_LENGTH(bd.`description`), 0),
+            'seoDescriptionLength', COALESCE(CHAR_LENGTH(bd.`seo_description`), 0),
+            'seoKeywords',          bd.`seo_keywords`,
+            'hasFooter',            CHAR_LENGTH(COALESCE(bd.`footer`, '')) > 2,
+            'hasFaq',               CHAR_LENGTH(COALESCE(bd.`faq`, '')) > 2,
+            'hasHowTo',             CHAR_LENGTH(COALESCE(bd.`how_to`, '')) > 2
+          )
+        ) AS seo
+      FROM " . DB_PREFIX . "blog_tag_description bd
+      JOIN " . DB_PREFIX . "blog_tag_to_store b2s
+        ON b2s.`blog_tag_id` = bd.`blog_tag_id`
+      WHERE bd.`language_id` = {$languageId}
+        AND b2s.`store_id`   = {$storeId}
+      {$ordering}
+      {$limits}
     ";
 
-    $this->db->query($sql);
+    foreach($this->db->query($sql)->rows ?? [] as $row) {
+      $row['seo'] = json_decode($row['seo'], true);
+      $row['seo']['seoKeywords'] = count(array_filter(explode(',', $row['seo']['seoKeywords'])));
+      $row['image'] = $row['image'] ? (HTTPS_CATALOG . 'image/' . $row['image']) : (HTTPS_CATALOG . 'image/no_image.webp');
+
+      $result[] = $row;
+    }
 
     return $result;
   }
 
-  public function getCategoryDescriptions($pageId) : array {
+  public function getTagDescription($pageId) : array {
     $pageId = (int) $pageId;
     $storeId = (int) $this->session->data['store_id'];
     $sql = "
       SELECT
         *
-      FROM " . DB_PREFIX . "blog_category_description bcd
-      JOIN " . DB_PREFIX . "blog_category_to_store bc2s
-        ON bc2s.category_id = bcd.category_id
-        AND bc2s.store_id = {$storeId}
-      WHERE bcd.category_id = {$pageId}
+      FROM " . DB_PREFIX . "blog_tag_description bd
+      JOIN " . DB_PREFIX . "blog_tag_to_store b2s
+        ON b2s.`blog_tag_id` = bd.`blog_tag_id`
+        AND b2s.store_id = {$storeId}
+      WHERE bd.`blog_tag_id` = {$pageId}
     ";
     
     return $this->db->query($sql)->rows ?? [];
   }
 
-  public function getCategoryTotal() : int {
+  public function getTagTotal() : int {
     $storeId = (int) $this->session->data['store_id'];
     $query = $this->db->query("
       SELECT
         COUNT(*) AS pages_count
-      FROM " . DB_PREFIX . "blog_category_description bcd
-      JOIN " . DB_PREFIX . "blog_category_to_store bc2s
-        ON bc2s.category_id = bcd.category_id
-        AND bc2s.store_id = {$storeId}
+      FROM " . DB_PREFIX . "blog_tag_description bd
+      JOIN " . DB_PREFIX . "blog_tag_to_store b2s
+        ON b2s.`blog_tag_id` = bd.`blog_tag_id`
+        AND b2s.store_id = {$storeId}
     ");
 
     return (int) ($query->row['pages_count'] ?? 0);
   }
 
+  
+  /**
+   * Filter array recursively and remove empty key => value pairs
+   * @param array $array The array to be affected
+   * @param array $deletedKeys The array of keys that will be treated as empty if all other keys are empty on this level 
+   * @return array
+   */
+  public function filterArrayRecursively(array $array = [], array $deletedKeys = []): array {
+    $filtered = [];
+
+    foreach ($array as $key => $value) {
+      if (is_array($value)) {
+        $value = $this->filterArrayRecursively($value, $deletedKeys);
+        if (!empty($value)) {
+          $filtered[$key] = $value;
+        }
+      // $value !== '&lt;p&gt;&lt;br&gt;&lt;/p&gt;' is a workaround for empty Summernote editor which always places this string: '<p><br></p>'
+      } elseif (trim((string) $value) !== '' && $value !== '&lt;p&gt;&lt;br&gt;&lt;/p&gt;') {
+        $filtered[$key] = $value;
+      }
+    }
+
+    // If result array is not empty, but only includes deletedKeys - then clear them also
+    if (!empty($filtered)) {
+      $nonDeletedKeys = array_diff(array_keys($filtered), $deletedKeys);
+
+      // Check recursively
+      $hasMeaningfulData = !empty($nonDeletedKeys);
+      foreach ($filtered as $key => $value) {
+        if (is_array($value) && !empty($value)) {
+          $hasMeaningfulData = true;
+          break;
+        }
+      }
+
+      if (!$hasMeaningfulData) {
+        return [];
+      }
+    }
+
+    return $filtered;
+  }
 }
