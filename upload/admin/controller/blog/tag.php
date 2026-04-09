279461e1d9fdfd58112764e7f575c82349596711
Tehnikus <tehnikus.ua@gmail.com>
2026-04-09 11:41:55 +0300

    Update Blog tags controller

--- upload/admin/controller/blog/tag.php
+++ upload/admin/controller/blog/tag.php
@@ -1,120 +1,141 @@
 <?php
-class ControllerBlogCategory extends Controller {
+class ControllerBlogTag extends Controller {
+  
+  private $error = [];
   public function index() {
-    $this->load->language('blog/category');
-    $this->load->model('blog/category');
+    $this->load->language('blog/tag');
+    $this->load->model('blog/tag');
     $this->document->setTitle($this->language->get('heading_title'));
-
+    
     $this->getList();
   }
-
+  
   public function getList() {
     $this->load->model('setting/store');
     $this->load->model('localisation/language');
-    $this->load->model('blog/category');
+    $this->load->model('blog/tag');
     $user_token = $this->session->data['user_token'];
+    $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['page', 'order'])));
+    
+    // Get items list
+    $filter_data = array(
+      'sort'  => $this->request->get['sort'] ?? 'date_added',
+      'order' => $this->request->get['order'] ?? 'DESC',
+      'start' => (($this->request->get['page'] ?? 1) - 1) * $this->config->get('config_limit_admin'),
+      'limit' => $this->config->get('config_limit_admin')
+    );
+    $items = $this->model_blog_tag->getList($filter_data);
+    // Set each item edit link
+    foreach ($items as $key => $item) {
+      $items[$key]['edit'] = $this->url->link('blog/tag/edit', 'blog_tag_id=' . $item['blog_tag_id'] . '&user_token=' . $user_token, true);
+    }
 
     $data = [
-      'categories'    => [],
-      'add'           => $this->url->link('blog/category/add', 'user_token=' . $user_token, true),
-      'delete'        => $this->url->link('blog/category/delete', 'user_token=' . $user_token, true),
-      'user_token'    => $user_token,
-      'breadcrumbs'   => $this->displayBreadcrumbs(),
-      'pagination'    => $this->getPaginamtion()['pagination'],
-      'results'       => $this->getPaginamtion()['results'],
+      'items'         => $items,
       'column_left'   => $this->load->controller('common/column_left'),
       'footer'        => $this->load->controller('common/footer'),
       'header'        => $this->load->controller('common/header'),
+      'breadcrumbs'   => $this->displayBreadcrumbs(),
+      'pagination'    => $this->getPagination()['pagination'],
+      'results'       => $this->getPagination()['results'],
+      'user_token'    => $user_token,
+      'add'           => $this->url->link('blog/tag/add', 'user_token=' . $user_token, true),
+      'delete'        => $this->url->link('blog/tag/delete', 'user_token=' . $user_token, true),
+      'sort'          => $this->request->get['sort'] ?? 'date_added',
+      'sort_name'     => $this->url->link('blog/tag', 'user_token=' . $user_token . $this->getSortOrder('name') . $url, true),
     ];
-
-    $this->response->setOutput($this->load->view('blog/category_list', $data));
+    
+    $this->response->setOutput($this->load->view('blog/tag_list', $data));
   }
-
+  
   public function getForm() : void {
     $this->load->model('setting/store');
     $this->load->model('localisation/language');
-    $this->load->model('blog/category');
+    $this->load->model('blog/tag');
+    $this->load->language('blog/tag');
     
     $data       = [];
-    $id         = $this->request->get['category_id'] ?? null;
+    $id         = $this->request->get['blog_tag_id'] ?? null;
     $user_token = $this->session->data['user_token'];
     $url        = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
-
+    
     $data = [
-      'description'   => $this->model_blog_category->getCategoryDescriptions($id),
       'column_left'   => $this->load->controller('common/column_left'),
       'footer'        => $this->load->controller('common/footer'),
       'header'        => $this->load->controller('common/header'),
       'languages'     => $this->model_localisation_language->getLanguages(),
       'stores'        => $this->model_setting_store->getMultistores(),
-      'action'        => $id ? $this->url->link('catalog/category/edit', 'user_token=' . $user_token . '&category_id=' . $id . $url, true) : $this->url->link('catalog/category/add', 'user_token=' . $user_token . $url, true),
-      'cancel'        => $this->url->link('catalog/category', 'user_token=' . $user_token . $url, true),
+      'action'        => $id ? $this->url->link('blog/tag/edit', 'user_token=' . $user_token . '&blog_tag_id=' . $id . $url, true) : $this->url->link('blog/tag/add', 'user_token=' . $user_token . $url, true),
+      'cancel'        => $this->url->link('blog/tag', 'user_token=' . $user_token . $url, true),
     ];
-    $this->response->setOutput($this->load->view('blog/category_form', $data));
+    
+    // Merge with errors array to hihlight faulty inputs. Merge saved data, POST data, errors and interface
+    $data = [...$data, ...$this->error, ...$this->getFormData()];
+    
+    $this->response->setOutput($this->load->view('blog/tag_form', $data));
   }
-
+  
+  public function getFormData() : array {
+    $formData       = [];
+    $blog_tag_id = $this->request->get['blog_tag_id'] ?? null;
+    $this->load->model('blog/tag');
+    $formData['tag_description'] = $this->model_blog_tag->getTagDescription($blog_tag_id);
+    // Replace actual data with POST data
+    if ($this->request->server['REQUEST_METHOD'] == 'POST') {
+      $formData = $this->request->post;
+    }
+    return $formData;
+  }
+  
   public function add() {
-    $this->load->language('blog/category');
-
-		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('blog/category');
-
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-			$this->model_blog_category->addCategory($this->request->post);
-			$this->session->data['success'] = $this->language->get('text_success');
+    $this->load->language('blog/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('blog/tag');
+    
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+      $this->model_blog_tag->addTag($this->request->post);
+      $this->session->data['success'] = $this->language->get('text_success');
       $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
-
-			$this->response->redirect($this->url->link('blog/category', 'user_token=' . $this->session->data['user_token'] . $url, true));
-		}
-
-		$this->getForm();
+      $this->response->redirect($this->url->link('blog/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getForm();
   }
-
-	public function edit() {
-		$this->load->language('blog/category');
-
-		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('blog/category');
-
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-			$this->model_blog_category->editCategory($this->request->get['category_id'], $this->request->post);
-
-			$this->session->data['success'] = $this->language->get('text_success');
-
+  
+  public function edit() {
+    $this->load->language('blog/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('blog/tag');
+    
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+      $this->model_blog_tag->editTag($this->request->get['blog_tag_id'], $this->request->post);
+      $this->session->data['success'] = $this->language->get('text_success');
       $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
-
-			$this->response->redirect($this->url->link('blog/category', 'user_token=' . $this->session->data['user_token'] . $url, true));
-		}
-
-		$this->getForm();
-	}
-
-	public function delete() {
-		$this->load->language('blog/category');
-
-		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('blog/category');
-
-		if (isset($this->request->post['selected']) && $this->validateDelete()) {
-			foreach ($this->request->post['selected'] as $category_id) {
-				$this->model_blog_category->deleteCategory($category_id);
-			}
-
-			$this->session->data['success'] = $this->language->get('text_success_deleted');
-
+      $this->response->redirect($this->url->link('blog/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getForm();
+  }
+  
+  public function delete() {
+    $this->load->language('blog/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('blog/tag');
+    
+    if (isset($this->request->post['selected']) && $this->validateDelete()) {
+      foreach ($this->request->post['selected'] as $blog_tag_id) {
+        $this->model_blog_tag->deleteTag($blog_tag_id);
+      }
+      $this->session->data['success'] = $this->language->get('text_success_deleted');
       $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
-
-			$this->response->redirect($this->url->link('blog/category', 'user_token=' . $this->session->data['user_token'] . $url, true));
-		}
-
-		$this->getList();
-	}
-
+      $this->response->redirect($this->url->link('blog/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getList();
+  }
+  
   public function displayBreadcrumbs() {
-    $this->load->language('blog/category');
+    $this->load->language('blog/tag');
     $breadcrumbs = [];
     $breadcrumbs[] = [
       'text' => $this->language->get('text_home'),
@@ -122,18 +143,17 @@ class ControllerBlogCategory extends Controller {
     ];
     $breadcrumbs[] = [
       'text' => $this->language->get('heading_title'),
-      'href' => $this->url->link('blog/category', 'user_token=' . $this->session->data['user_token'], true)
+      'href' => $this->url->link('blog/tag', 'user_token=' . $this->session->data['user_token'], true)
     ];
+    
     return $breadcrumbs;
   }
-
-  public function getPaginamtion() {
-    $this->load->model('blog/category');
-    $total = $this->model_blog_category->getCategoryTotal();
+  
+  public function getPagination() {
+    $this->load->model('blog/tag');
+    $total = $this->model_blog_tag->getTagTotal();
     $page = (int) ($this->request->get['page'] ?? 1);
-
     $params = ['sort', 'order'];
-
     $query = [];
     
     foreach ($params as $param) {
@@ -143,66 +163,93 @@ class ControllerBlogCategory extends Controller {
     }
     
     $url = $query ? '&' . http_build_query($query) : '';
-
     $pagination = new Pagination();
-		$pagination->total = $total;
-		$pagination->page  = $page;
-		$pagination->limit = $this->config->get('config_limit_admin');
-		$pagination->url = $this->url->link('blog/category', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
-
-		$data['pagination'] = $pagination->render();
-		$data['results'] = sprintf($this->language->get('text_pagination'), ($total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($total - $this->config->get('config_limit_admin'))) ? $total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $total, ceil($total / $this->config->get('config_limit_admin')));
-
+    $pagination->total = $total;
+    $pagination->page  = $page;
+    $pagination->limit = $this->config->get('config_limit_admin');
+    $pagination->url = $this->url->link('blog/tag', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
+    $data['pagination'] = $pagination->render();
+    $data['results'] = sprintf($this->language->get('text_pagination'), ($total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($total - $this->config->get('config_limit_admin'))) ? $total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $total, ceil($total / $this->config->get('config_limit_admin')));
+    
     return $data;
+  }
+  
+  private function getSortOrder(string $column): string {
+    $currentSort  = $this->request->get['sort'] ?? '';
+    $currentOrder = $this->request->get['order'] ?? 'ASC';
+    
+    if ($currentSort === $column) {
+      $order = ($currentOrder === 'ASC') ? 'DESC' : 'ASC';
+    } else {
+      $order = 'ASC';
+    }
+    
+    return '&sort=' . $column . '&order=' . $order;
   }
 
   protected function validateForm() {
-		if (!$this->user->hasPermission('modify', 'blog/category')) {
-			$this->error['warning'] = $this->language->get('error_permission');
+		if (!$this->user->hasPermission('modify', 'blog/tag')) {
+			$this->error['warning'] = $this->language->get('e_permission');
 		}
 
-		foreach ($this->request->post['category_description'] as $language_id => $value) {
+		foreach ($this->request->post['tag_description'] as $language_id => $value) {
 			if ((utf8_strlen($value['name']) < 1) || (utf8_strlen($value['name']) > 255)) {
-				$this->error['name'][$language_id] = $this->language->get('error_name');
+				$this->error['error_name'][$language_id] = $this->language->get('e_name');
 			}
 		}
 
-		// if ($this->request->post['category_seo_url']) {
-		// 	$this->load->model('design/seo_url');
+    // // Check if parent category is selected
+    // if (!isset($this->request->post['filter_page_facet'][1])) {
+    //   $this->error['error_select_category'] = $this->language->get('e_select_category');
+    // }
 
-		// 	foreach ($this->request->post['category_seo_url'] as $store_id => $language) {
-		// 		foreach ($language as $language_id => $keyword) {
-		// 			if (!empty($keyword)) {
-		// 				if (count(array_keys($language, $keyword)) > 1) {
-		// 					$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_unique');
-		// 				}
 
-		// 				$seo_urls = $this->model_design_seo_url->getSeoUrlsByKeyword($keyword);
+		if ($this->request->post['seo_url']) {
 
-		// 				foreach ($seo_urls as $seo_url) {
-		// 					if (($seo_url['store_id'] == $store_id) && (!isset($this->request->get['category_id']) || ($seo_url['query'] != 'category_id=' . $this->request->get['category_id']))) {
-		// 						$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_keyword');
+			$this->load->model('design/seo_url');
+      $storeId = (int) $this->session->data['store_id'];
 
-		// 						break;
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
+      foreach ($this->request->post['seo_url'] as $langId => $currentUrl) {
+
+        $currentUrl = trim(mb_strtolower($currentUrl));
+        if (!$currentUrl) continue;
+
+        $pageRequest = $this->model_design_seo_url->buildQuery(
+          $this->request->post['filter_page_facet'] ?? []
+        );
+
+        $isUrlExists = $this->model_design_seo_url->checkUrlDuplicate($currentUrl, $langId, $storeId);
+        $isRequestExists = $this->model_design_seo_url->checkRequestDuplicate($pageRequest, $langId, $storeId);
+
+        foreach ($isUrlExists ?? [] as $row) {
+          if ($row['query'] !== $pageRequest) {
+            $this->error['error_url_not_unique'][$langId] = $this->language->get('e_url_not_unique');
+            break;
+          }
+        }
+
+        foreach ($isRequestExists ?? [] as $row) {
+          if ($row['keyword'] !== $currentUrl) {
+            $this->error['error_request_not_unique'][$langId] = sprintf($this->language->get('e_request_not_unique'), $row['keyword']);
+            break;
+          }
+        }
+      }
+		}
 
 		if ($this->error && !isset($this->error['warning'])) {
-			$this->error['warning'] = $this->language->get('error_warning');
+			$this->error['warning'] = $this->language->get('e_warning');
 		}
+
+    // echo '<pre>' . htmlspecialchars(print_r($this->error, true)) . '</pre>';
 
 		return !$this->error;
 	}
-
-	protected function validateDelete() {
-		if (!$this->user->hasPermission('modify', 'blog/category')) {
-			$this->error['warning'] = $this->language->get('error_permission');
-		}
-
-		return !$this->error;
-	}
+  
+  protected function validateDelete() {
+    if (!$this->user->hasPermission('modify', 'blog/tag')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
+    return !$this->error;
+  }
 }
