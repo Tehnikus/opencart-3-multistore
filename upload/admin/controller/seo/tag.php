6d655e315cdacd5048d80b7337a8f96e4a05370c
Tehnikus <tehnikus.ua@gmail.com>
2026-04-10 14:40:39 +0300

    Add SEO tags model, controller, list and form

---
+++ upload/admin/controller/seo/tag.php
@@ -0,0 +1,257 @@
+<?php
+class ControllerSeoTag extends Controller {
+  
+  private $error = [];
+  public function index() {
+    $this->load->language('seo/tag');
+    $this->load->model('seo/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    
+    $this->getList();
+  }
+  
+  public function getList() : void {
+    $this->load->model('setting/store');
+    $this->load->model('localisation/language');
+    $this->load->model('seo/tag');
+    $user_token = $this->session->data['user_token'];
+    $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['page', 'order'])));
+    
+    // Get items list
+    $filter_data = array(
+      'sort'  => $this->request->get['sort'] ?? 'date_added',
+      'order' => $this->request->get['order'] ?? 'DESC',
+      'start' => (($this->request->get['page'] ?? 1) - 1) * $this->config->get('config_limit_admin'),
+      'limit' => $this->config->get('config_limit_admin')
+    );
+    $items = $this->model_seo_tag->getList($filter_data);
+    // Set each item edit link
+    foreach ($items as $key => $item) {
+      $items[$key]['edit'] = $this->url->link('seo/tag/edit', 'seo_tag_id=' . $item['seo_tag_id'] . '&user_token=' . $user_token, true);
+    }
+
+    $data = [
+      'items'         => $items,
+      'column_left'   => $this->load->controller('common/column_left'),
+      'footer'        => $this->load->controller('common/footer'),
+      'header'        => $this->load->controller('common/header'),
+      'breadcrumbs'   => $this->displayBreadcrumbs(),
+      'pagination'    => $this->getPagination()['pagination'],
+      'results'       => $this->getPagination()['results'],
+      'user_token'    => $user_token,
+      'add'           => $this->url->link('seo/tag/add', 'user_token=' . $user_token, true),
+      'delete'        => $this->url->link('seo/tag/delete', 'user_token=' . $user_token, true),
+      'sort'          => $this->request->get['sort'] ?? 'date_added',
+      'sort_name'     => $this->url->link('seo/tag', 'user_token=' . $user_token . $this->getSortOrder('name') . $url, true),
+    ];
+    
+    $this->response->setOutput($this->load->view('seo/tag_list', $data));
+  }
+  
+  public function getForm() : void {
+    $this->load->model('setting/store');
+    $this->load->model('localisation/language');
+    $this->load->model('seo/tag');
+    $this->load->language('seo/tag');
+    
+    $data       = [];
+    $id         = $this->request->get['seo_tag_id'] ?? null;
+    $user_token = $this->session->data['user_token'];
+    $url        = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
+    
+    $data = [
+      'column_left'   => $this->load->controller('common/column_left'),
+      'footer'        => $this->load->controller('common/footer'),
+      'header'        => $this->load->controller('common/header'),
+      'languages'     => $this->model_localisation_language->getLanguages(),
+      'stores'        => $this->model_setting_store->getMultistores(),
+      'action'        => $id ? $this->url->link('seo/tag/edit', 'user_token=' . $user_token . '&seo_tag_id=' . $id . $url, true) : $this->url->link('seo/tag/add', 'user_token=' . $user_token . $url, true),
+      'cancel'        => $this->url->link('seo/tag', 'user_token=' . $user_token . $url, true),
+    ];
+    
+    // Merge with errors array to hihlight faulty inputs. Merge saved data, POST data, errors and interface
+    $data = [...$data, ...$this->error, ...$this->getFormData()];
+    
+    $this->response->setOutput($this->load->view('seo/tag_form', $data));
+  }
+  
+  public function getFormData() : array {
+    $formData       = [];
+    $seo_tag_id = $this->request->get['seo_tag_id'] ?? null;
+    $this->load->model('seo/tag');
+    $formData['seo_tag_description'] = $this->model_seo_tag->getTagDescription($seo_tag_id);
+    $formData['seo_url']             = $this->model_seo_tag->getSeoUrl($seo_tag_id);
+    // Replace actual data with POST data
+    if ($this->request->server['REQUEST_METHOD'] == 'POST') {
+      $formData = $this->request->post;
+    }
+    return $formData;
+  }
+  
+  public function add() : void {
+    $this->load->language('seo/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('seo/tag');
+    
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+      $this->model_seo_tag->addTag($this->request->post);
+      $this->session->data['success'] = $this->language->get('text_success');
+      $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
+      $this->response->redirect($this->url->link('seo/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getForm();
+  }
+  
+  public function edit() : void {
+    $this->load->language('seo/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('seo/tag');
+    
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+      $this->model_seo_tag->editTag($this->request->get['seo_tag_id'], $this->request->post);
+      $this->session->data['success'] = $this->language->get('text_success');
+      $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
+      $this->response->redirect($this->url->link('seo/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getForm();
+  }
+  
+  public function delete() : void {
+    $this->load->language('seo/tag');
+    $this->document->setTitle($this->language->get('heading_title'));
+    $this->load->model('seo/tag');
+    
+    if (isset($this->request->post['selected']) && $this->validateDelete()) {
+      foreach ($this->request->post['selected'] as $seo_tag_id) {
+        $this->model_seo_tag->deleteTag($seo_tag_id);
+      }
+      $this->session->data['success'] = $this->language->get('text_success_deleted');
+      $url = '&' . http_build_query(array_intersect_key($this->request->get, array_flip(['sort', 'order', 'page'])));
+      $this->response->redirect($this->url->link('seo/tag', 'user_token=' . $this->session->data['user_token'] . $url, true));
+    }
+    
+    $this->getList();
+  }
+  
+  public function displayBreadcrumbs() : array {
+    $this->load->language('seo/tag');
+    $breadcrumbs = [];
+    $breadcrumbs[] = [
+      'text' => $this->language->get('text_home'),
+      'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+    ];
+    $breadcrumbs[] = [
+      'text' => $this->language->get('heading_title'),
+      'href' => $this->url->link('seo/tag', 'user_token=' . $this->session->data['user_token'], true)
+    ];
+    
+    return $breadcrumbs;
+  }
+  
+  public function getPagination() : array {
+    $this->load->model('seo/tag');
+    $total = $this->model_seo_tag->getTagTotal();
+    $page = (int) ($this->request->get['page'] ?? 1);
+    $params = ['sort', 'order'];
+    $query = [];
+    
+    foreach ($params as $param) {
+      if (isset($this->request->get[$param])) {
+        $query[$param] = $this->request->get[$param];
+      }
+    }
+    
+    $url = $query ? '&' . http_build_query($query) : '';
+    $pagination = new Pagination();
+    $pagination->total = $total;
+    $pagination->page  = $page;
+    $pagination->limit = $this->config->get('config_limit_admin');
+    $pagination->url = $this->url->link('seo/tag', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
+    $data['pagination'] = $pagination->render();
+    $data['results'] = sprintf($this->language->get('text_pagination'), ($total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($total - $this->config->get('config_limit_admin'))) ? $total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $total, ceil($total / $this->config->get('config_limit_admin')));
+    
+    return $data;
+  }
+  
+  private function getSortOrder(string $column): string {
+    $currentSort  = $this->request->get['sort'] ?? '';
+    $currentOrder = $this->request->get['order'] ?? 'ASC';
+    
+    if ($currentSort === $column) {
+      $order = ($currentOrder === 'ASC') ? 'DESC' : 'ASC';
+    } else {
+      $order = 'ASC';
+    }
+    
+    return '&sort=' . $column . '&order=' . $order;
+  }
+
+  protected function validateForm() : bool {
+		if (!$this->user->hasPermission('modify', 'seo/tag')) {
+			$this->error['warning'] = $this->language->get('e_permission');
+		}
+
+		foreach ($this->request->post['seo_tag_description'] as $language_id => $value) {
+			if ((utf8_strlen($value['name']) < 1) || (utf8_strlen($value['name']) > 255)) {
+				$this->error['error_name'][$language_id] = $this->language->get('e_name');
+			}
+    }
+
+		if ($this->request->post['seo_url']) {
+
+			$this->load->model('design/seo_url');
+      $storeId = (int) $this->session->data['store_id'];
+
+      foreach ($this->request->post['seo_url'] as $langId => $currentUrl) {
+
+        $currentUrl = trim(mb_strtolower($currentUrl));
+        if (!$currentUrl) continue;
+
+        $pageRequest = (isset($this->request->get['seo_tag_id'])) ? 'seo_tag_id=' . ((int) $this->request->get['seo_tag_id']) : '';
+
+        $isUrlExists = $this->model_design_seo_url->checkUrlDuplicate($currentUrl, $langId, $storeId);
+        $isRequestExists = $this->model_design_seo_url->checkRequestDuplicate($pageRequest, $langId, $storeId);
+
+        foreach ($isUrlExists ?? [] as $row) {
+          if ($row['query'] !== $pageRequest) {
+            $this->error['error_url_not_unique'][$langId] = $this->language->get('e_url_not_unique');
+            break;
+          }
+        }
+
+        foreach ($isRequestExists ?? [] as $row) {
+          if ($row['keyword'] !== $currentUrl) {
+            $this->error['error_request_not_unique'][$langId] = sprintf($this->language->get('e_request_not_unique'), $row['keyword']);
+            break;
+          }
+        }
+      }
+		}
+
+		if ($this->error && !isset($this->error['warning'])) {
+			$this->error['warning'] = $this->language->get('e_warning');
+		}
+
+		return !$this->error;
+	}
+  
+  protected function validateDelete() : bool {
+    if (!$this->user->hasPermission('modify', 'seo/tag')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
+    return !$this->error;
+  }
+
+  public function autocomplete() : void {
+    $post = $this->request->post;
+    // $search = $post['search'] ?? [];
+    
+    $this->load->model('seo/tag');
+    $tags = $this->model_seo_tag->getList($post);
+
+    $this->response->addHeader('Content-Type: application/json');
+    $this->response->setOutput(json_encode($tags));
+  }
+}
---
+++ upload/admin/model/seo/tag.php
@@ -0,0 +1,241 @@
+<?php
+class ModelSeoTag extends Model {
+
+  private $sortOrders = [
+    'name'          => 'bd.`name`',
+    'date_modified' => 'b2s.`date_modified`',
+  ];
+
+  public function addTag($data) {
+
+    $this->db->query("START TRANSACTION");
+
+    try {
+
+      // Save main table
+      $this->db->query("
+        INSERT INTO " . DB_PREFIX . "seo_tag_to_store 
+        SET
+          `store_id` 			 = '" . (int) $this->session->data['store_id'] . "', 
+          `date_added`     = NOW(),
+          `date_modified`  = NOW()
+        ");
+
+      $seo_tag_id = $this->db->getLastId();
+
+      // Save descriptions
+      foreach ($data['seo_tag_description'] as $language_id => $value) {
+        $this->db->query("
+          INSERT INTO " . DB_PREFIX . "seo_tag_description 
+          SET 
+            `seo_tag_id` 		  = '" . (int) $seo_tag_id . "', 
+            `language_id` 			= '" . (int) $language_id . "', 
+            `store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+            `name` 							= '" . $this->db->escape($value['name']) . "' 
+        ");
+      }
+
+      // Save URL
+      foreach ($data['seo_url'] ?? [] as $language_id => $keyword) {
+        // Mostly safety delete, should never happen
+        $this->db->query("
+          DELETE FROM " . DB_PREFIX . "seo_url 
+          WHERE `query`       = 'seo_tag_id=" . $seo_tag_id . "'
+            AND `language_id` = '" . (int) $language_id . "'
+            AND `store_id`    = '" . (int) $this->session->data['store_id'] . "'
+        ");
+
+        if (!empty($keyword)) {
+          $this->db->query("
+            INSERT INTO " . DB_PREFIX . "seo_url 
+            SET 
+              `store_id`    = '" . (int) $this->session->data['store_id'] . "',
+              `language_id` = '" . (int) $language_id . "', 
+              `query`       = 'seo_tag_id=" . $seo_tag_id . "', 
+              `keyword`     = '" . $this->db->escape($keyword) . "'
+          ");
+        }
+      }
+      
+      $this->db->query("COMMIT");
+      return $seo_tag_id;
+      
+    } catch (\Throwable $e) {
+      $this->db->query("ROLLBACK");
+      throw $e;
+    }
+  }
+
+  public function editTag($seo_tag_id, $data) {
+    
+    $this->db->query("START TRANSACTION");
+
+    try {
+
+      // Update main table
+      $this->db->query("
+        UPDATE " . DB_PREFIX . "seo_tag_to_store 
+        SET
+          `date_modified`  = NOW()
+        WHERE seo_tag_id = " . (int) $seo_tag_id . "
+      ");
+
+      // Update descriptions
+      $this->db->query("
+        DELETE FROM " . DB_PREFIX . "seo_tag_description
+        WHERE `seo_tag_id` = " . (int) $seo_tag_id . "
+      ");
+
+      foreach ($data['seo_tag_description'] as $language_id => $value) {
+        $this->db->query("
+          INSERT INTO " . DB_PREFIX . "seo_tag_description 
+          SET 
+            `seo_tag_id` 		  = '" . (int) $seo_tag_id . "', 
+            `language_id` 			= '" . (int) $language_id . "', 
+            `store_id` 					= '" . (int) $this->session->data['store_id'] . "',
+            `name` 							= '" . $this->db->escape($value['name']) . "'
+        ");
+      }
+
+      // Save URL
+      foreach ($data['seo_url'] ?? [] as $language_id => $keyword) {
+        // Mostly safety delete, should never happen
+        $this->db->query("
+          DELETE FROM " . DB_PREFIX . "seo_url 
+          WHERE `query`       = 'seo_tag_id=" . $seo_tag_id . "'
+            AND `language_id` = '" . (int) $language_id . "'
+            AND `store_id`    = '" . (int) $this->session->data['store_id'] . "'
+        ");
+
+        if (!empty($keyword)) {
+          $this->db->query("
+            INSERT INTO " . DB_PREFIX . "seo_url 
+            SET 
+              `store_id`    = '" . (int) $this->session->data['store_id'] . "',
+              `language_id` = '" . (int) $language_id . "', 
+              `query`       = 'seo_tag_id=" . $seo_tag_id . "', 
+              `keyword`     = '" . $this->db->escape($keyword) . "'
+          ");
+        }
+      }
+      
+      $this->db->query("COMMIT");
+      return $seo_tag_id;
+      
+    } catch (\Throwable $e) {
+      $this->db->query("ROLLBACK");
+      throw $e;
+    }
+  }
+
+  public function getList($filter) : array {
+    $result     = [];
+    $storeId    = (int) $this->session->data['store_id'];
+    $languageId = (int) $this->config->get('config_language_id');
+    
+    // Orders
+    $ordering = '';
+    $sortField = 'b2s.`date_modified`';
+
+    if (!empty($filter['sort']) && isset($this->sortOrders[$filter['sort']])) {
+      $sortField = $this->sortOrders[$filter['sort']];
+    }
+
+    $orderDirection = 'DESC';
+    if (!empty($filter['order']) && in_array($filter['order'], ['ASC', 'DESC'])) {
+      $orderDirection = $filter['order'];
+    }
+
+    $ordering = "ORDER BY {$sortField} {$orderDirection}";
+      
+    // Limits
+    $limit  = max(1, (int) ($filter['limit'] ?? $this->config->get('config_limit_admin') ?? 20));
+    $start  = max(0, (int) ($filter['start'] ?? 0));
+    $limits = " LIMIT {$start}, {$limit}";
+
+    $sql = "
+      SELECT
+        bd.`seo_tag_id`,
+        bd.`name`,
+        b2s.`date_modified`
+      FROM " . DB_PREFIX . "seo_tag_description bd
+      JOIN " . DB_PREFIX . "seo_tag_to_store b2s
+        ON b2s.`seo_tag_id` = bd.`seo_tag_id`
+      WHERE bd.`language_id` = {$languageId}
+        AND b2s.`store_id`   = {$storeId}
+        " . ((isset($filter['name'])) ? "AND bd.name LIKE '%" . $this->db->escape($filter['name']) . "%'" : '') . "
+      {$ordering}
+      {$limits}
+    ";
+
+    foreach($this->db->query($sql)->rows ?? [] as $row) {
+      $row['seo'] = json_decode($row['seo'], true);
+      $row['seo']['seoKeywords'] = count(array_filter(explode(',', $row['seo']['seoKeywords'])));
+      $row['image'] = $row['image'] ? (HTTPS_CATALOG . 'image/' . $row['image']) : (HTTPS_CATALOG . 'image/no_image.webp');
+
+      $result[] = $row;
+    }
+
+    return $result;
+  }
+
+  public function getSeoUrl($seo_tag_id = null) : array {
+    $result   = [];
+    $store_id = (int) $this->session->data['store_id'];
+
+    if ($seo_tag_id === null) {
+      return $result;
+    }
+
+    $query = $this->db->query("
+      SELECT
+        *
+      FROM " . DB_PREFIX . "seo_url su
+      WHERE su.`query`    = 'seo_tag_id=" . (int) $seo_tag_id . "'
+        AND su.`store_id` = " . $store_id . "
+    ")->rows;
+
+    foreach ($query as $row) {
+      $result[$row['language_id']] = $row['keyword'];
+    }
+
+    return $result;
+  }
+
+  public function getTagDescription($pageId) : array {
+    $pageId = (int) $pageId;
+    $storeId = (int) $this->session->data['store_id'];
+    $sql = "
+      SELECT
+        *
+      FROM " . DB_PREFIX . "seo_tag_description bd
+      JOIN " . DB_PREFIX . "seo_tag_to_store b2s
+        ON b2s.`seo_tag_id` = bd.`seo_tag_id`
+        AND b2s.store_id = {$storeId}
+      WHERE bd.`seo_tag_id` = {$pageId}
+    ";
+    
+    foreach($this->db->query($sql)->rows ?? [] as $row) {
+      $row['footer'] = json_decode($row['footer'] ?? '[]', true);
+      $row['faq']    = json_decode($row['faq'] ?? '[]', true);
+      $row['how_to'] = json_decode($row['how_to'] ?? '[]', true);
+      $result[$row['language_id']] = $row;
+    }
+    
+    return $result ?? [];
+  }
+
+  public function getTagTotal() : int {
+    $storeId = (int) $this->session->data['store_id'];
+    $query = $this->db->query("
+      SELECT
+        COUNT(*) AS pages_count
+      FROM " . DB_PREFIX . "seo_tag_description bd
+      JOIN " . DB_PREFIX . "seo_tag_to_store b2s
+        ON b2s.`seo_tag_id` = bd.`seo_tag_id`
+        AND b2s.`store_id`  = {$storeId}
+    ");
+
+    return (int) ($query->row['pages_count'] ?? 0);
+  }
+}
