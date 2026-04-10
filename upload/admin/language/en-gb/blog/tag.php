a144b83837cbf7eaac5950d37a1ca4e29d93c0ce
Tehnikus <tehnikus.ua@gmail.com>
2026-04-10 14:44:51 +0300

    Add blog tags translations

---
+++ upload/admin/language/en-gb/blog/tag.php
@@ -0,0 +1,25 @@
+<?php
+$_['heading_title']             = 'Blog tags';
+$_['text_no_results']           = 'No results';
+$_['text_confirm']              = 'This will delete selected blog tags! Are you sure?';
+
+// Buttons
+$_['button_add']                = 'Add new blog tag';
+$_['button_delete']             = 'Delete';
+
+// Columns
+$_['column_name']               = 'Name';
+
+// Errors
+$_['e_name']                    = 'Set article name';
+$_['e_unique']                  = 'SEO URL is not unique';
+$_['e_permission']              = 'You are not permitted to edit blog tags';
+$_['e_warning']                 = 'Error while saving blog tag';
+
+// Messages
+$_['text_success']              = 'Blog tag saved';
+$_['text_success_deleted']      = 'Blog tag deleted';
+
+// Buttons
+$_['button_cancel']             = 'Cancel';
+$_['button_save']               = 'Save';
