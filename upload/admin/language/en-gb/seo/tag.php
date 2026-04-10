2b13b0b486ce0c79a55a8617e506ae607c28ee7f
Tehnikus <tehnikus.ua@gmail.com>
2026-04-10 14:41:29 +0300

    Add SEO tags translation

---
+++ upload/admin/language/en-gb/seo/tag.php
@@ -0,0 +1,25 @@
+<?php
+$_['heading_title']             = 'Seo tags';
+$_['text_no_results']           = 'No results';
+$_['text_confirm']              = 'This will delete selected seo tags! Are you sure?';
+
+// Buttons
+$_['button_add']                = 'Add new seo tag';
+$_['button_delete']             = 'Delete';
+
+// Columns
+$_['column_name']               = 'Name';
+
+// Errors
+$_['e_name']                    = 'Set article name';
+$_['e_unique']                  = 'SEO URL is not unique';
+$_['e_permission']              = 'You are not permitted to edit seo tags';
+$_['e_warning']                 = 'Error while saving seo tag';
+
+// Messages
+$_['text_success']              = 'Seo tag saved';
+$_['text_success_deleted']      = 'Seo tag deleted';
+
+// Buttons
+$_['button_cancel']             = 'Cancel';
+$_['button_save']               = 'Save';
