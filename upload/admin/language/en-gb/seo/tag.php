ffeead7fb624db3962c8d47c5e52d5dd7c82b866
Tehnikus <tehnikus.ua@gmail.com>
2026-04-13 13:59:23 +0300

    Update translations

--- upload/admin/language/en-gb/seo/tag.php
+++ upload/admin/language/en-gb/seo/tag.php
@@ -4,6 +4,7 @@ $_['text_no_results']           = 'No results';
 $_['text_confirm']              = 'This will delete selected seo tags! Are you sure?';
 $_['text_seo_tags_info']        = 'Here you can create tags to group unrelated products together. To use tags as SEO pages, create a "tag+category" combination in the "<b>SEO/Landing Pages</b>" section.';
 $_['text_show_as_flag']         = 'Show as product flag on product miniatures and product pages';
+$_['text_show_as_facet']        = 'Show as filter item in <b>Facet filter</b> block. Select products related to this tag to show them in filtering results. Be sure to fill SEO URL field so URLs are built correctly. Be sure that you have set <b>Show tags</b> in Facet filter settings for selected categories or globally for all categories';
 $_['text_inline_style']         = 'Add inline CSS styles here that will be outputted in <b>style="...your styles..."</b> in frontend';
 $_['text_inline_icon']          = 'Add icon or any small html code here to display before tag name in frontend. Be careful not to brake your page layout';
 $_['text_seo_tag_form']         = 'Tag settings';
@@ -12,7 +13,7 @@ $_['text_seo_tag_form']         = 'Tag settings';
 $_['entry_show_as_flag']        = 'Show as flag';
 $_['entry_inline_style']        = 'Inline CSS style';
 $_['entry_inline_icon']         = 'Inline icon';
-$_['entry_show_as_flag']        = 'Show as flag';
+$_['entry_show_as_facet']       = 'Show as filter';
 $_['entry_product_tag']         = 'Products';
 
 // Buttons
