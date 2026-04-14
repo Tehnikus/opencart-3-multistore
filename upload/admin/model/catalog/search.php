15154ceb9db5b8f2ce390a35b18af2ec8a27d0e6
Tehnikus <tehnikus.ua@gmail.com>
2026-04-14 16:47:13 +0300

    Model: Add rebuild index method

--- upload/admin/model/catalog/search.php
+++ upload/admin/model/catalog/search.php
@@ -1,4 +1,35 @@
 <?php
 class ModelCatalogSearch extends Model {
 
+  /**
+   * Full search index rebuild
+   */
+  public function rebuildIndex() : array {
+    $stats = [
+      'deleted' => 0,
+      'indexed' => 0,
+      'errors'  => [],
+    ];
+
+    $this->db->query("START TRANSACTION");
+
+    try {
+      // Clear index
+      // Truncate is faster than delte by row
+      $this->db->query("TRUNCATE TABLE `" . DB_PREFIX . "product_search_index`");
+      $stats['deleted'] = $this->db->countAffected();
+
+      // Build the whole index
+      $this->db->query($this->buildInsertQuery());
+      $stats['indexed'] = $this->db->countAffected();
+      $this->db->query("COMMIT");
+
+    } catch (Exception $e) {
+      $stats['errors'][] = $e->getMessage();
+      $this->db->query("ROLLBACK");
+    }
+
+    return $stats;
+  }
+
 }
