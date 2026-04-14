b124df66fc9eb6964e4a805ea28e70f0aa574eb1
Tehnikus <tehnikus.ua@gmail.com>
2026-04-14 16:48:17 +0300

    Model: Add buildSearchIndex() method

--- upload/admin/model/catalog/search.php
+++ upload/admin/model/catalog/search.php
@@ -32,4 +32,54 @@ class ModelCatalogSearch extends Model {
     return $stats;
   }
 
+  /**
+   * @param mixed $product_id If not isset the index will be created for all products in store with $store_id = n
+   * @param mixed $store_id If not isset the index for $product_id = n will be created for all stores
+   * If both params are not set the index will be created for all products in all stores
+   */
+  public function buildSearchIndex($product_id = null, $store_id = null) : void {
+    $where = [];
+    if ($product_id !== null) {
+      $where[] = "product_id = " . (int) $product_id;
+    }
+    if ($store_id !== null) {
+      $where[] = "store_id = " . (int) $store_id;
+    }
+
+    // DB Transaction
+    $this->db->query("START TRANSACTION");
+    try {
+      if ($product_id !== null) {
+
+        // First remove old data
+        $this->db->query("
+          DELETE FROM `" . DB_PREFIX . "product_search_index`
+          WHERE " . implode(' AND ', $where) . "
+        ");
+  
+        // Then check product status
+        $result = $this->db->query("
+          SELECT 
+            `status` 
+          FROM `" . DB_PREFIX . "product_to_store`
+          WHERE " . implode(' AND ', $where) . "
+        ");
+
+        // If product is disabled just exit
+        if (!$result->num_rows || !$result->row['status']) {
+          return;
+        }
+      }
+
+      // If product is available or product_id is null build search index
+      $this->db->query($this->buildInsertQuery($product_id, $store_id));
+      // Commit DB changes
+      $this->db->query("COMMIT");
+    } catch (Exception $e) {
+      // Rollback DB changes
+      $this->db->query("ROLLBACK");
+      throw $e;
+    }
+  }
+
 }
