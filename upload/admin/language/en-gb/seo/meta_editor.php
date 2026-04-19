dc0d336c3c17560567f7aa7fd9e3c307ca8742e0
Tehnikus <tehnikus.ua@gmail.com>
2026-04-19 18:09:26 +0300

    Update translations

--- upload/admin/language/en-gb/seo/meta_editor.php
+++ upload/admin/language/en-gb/seo/meta_editor.php
@@ -1,3 +1,22 @@
 <?php
 // Heading
 $_['meta_editor']             = 'SEO Meta editor';
+$_['header_category']         = 'Categories';
+$_['header_product']          = 'Products';
+$_['header_manufacturer']     = 'Manufacturers';
+$_['header_article']          = 'Articles';
+$_['header_tag']              = 'Blog tags';
+$_['header_filter_page']      = 'Landing pages';
+
+// Inputs
+$_['input_search']                  = 'Search';
+$_['input_h1']                      = 'H1';
+$_['input_meta_title']              = 'Title';
+$_['input_meta_description']        = 'Description';
+$_['input_meta_description_short']  = 'Desc';
+$_['input_faq']                     = 'FAQ';
+$_['input_how_to']                  = 'HowTo';
+$_['input_description']             = 'Description';
+$_['input_seo_description']         = 'SEO Description';
+$_['input_footer']                  = 'Footer';
+$_['input_seo_keywords']            = 'Keywords';
