d143d964f5739c1bf71a153806d36a4a516b9787
Tehnikus <tehnikus.ua@gmail.com>
2026-05-05 11:39:17 +0300

    Frontend: Add SEO tag to facet filter requests to check applied filters

--- upload/catalog/controller/extension/module/facet_filter.php
+++ upload/catalog/controller/extension/module/facet_filter.php
@@ -50,11 +50,12 @@ class ControllerExtensionModuleFacetFilter extends Controller {
 		
 		// Request data to check applied filters
 		$data['requests'] = [
+			'category_id' 			=> explode(',', $this->request->get['category_id'] ?? '') 			?? null,
 			'filter' 						=> explode(',', $this->request->get['filter'] ?? '') 						?? null,
 			'option' 						=> explode(',', $this->request->get['option'] ?? '') 						?? null,
 			'attribute' 				=> explode(',', $this->request->get['attribute'] ?? '') 				?? null,
 			'manufacturer_id' 	=> explode(',', $this->request->get['manufacturer_id'] ?? '') 	?? null,
-			'category_id' 			=> explode(',', $this->request->get['category_id'] ?? '') 			?? null,
+			'tag' 							=> explode(',', $this->request->get['tag'] ?? '') 							?? null,
 			'is_available' 			=> explode(',', $this->request->get['is_available'] ?? '') 			?? null,
 			'is_featured' 			=> explode(',', $this->request->get['is_featured'] ?? '') 			?? null,
 			'has_discount' 			=> explode(',', $this->request->get['has_discount'] ?? '') 			?? null,
