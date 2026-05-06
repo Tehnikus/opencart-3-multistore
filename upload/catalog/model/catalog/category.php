753923563e1eb4cd4436d0eec49e54ee0986b301
Tehnikus <tehnikus.ua@gmail.com>
2026-05-06 16:09:57 +0300

    Frontend: Model: Add additional images to main category SQL query

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -12,7 +12,7 @@ class ModelCatalogCategory extends Model {
 			return $cachedData;
 		}
 
-		$query = $this->db->query("
+		$sql = "
 			SELECT 
 				c.`category_id`,
 				c2s.`store_id`,
@@ -34,29 +34,45 @@ class ModelCatalogCategory extends Model {
 				cd.`footer`,
 				cd.`date_modified`,
 				cd.`language_id`,
-				(SELECT COUNT(fi.`product_id`) FROM " . DB_PREFIX . "facet_index fi WHERE fi.`facet_value_id` = c.`category_id` AND fi.`facet_type` = 1 AND fi.`store_id` = c2s.`store_id`) AS product_count
+				(SELECT COUNT(fi.`product_id`) FROM " . DB_PREFIX . "facet_index fi WHERE fi.`facet_value_id` = c.`category_id` AND fi.`facet_type` = 1 AND fi.`store_id` = c2s.`store_id`) AS product_count,
+				JSON_ARRAYAGG(
+					JSON_OBJECT(
+						'image', 				ci.`image`,
+						'sort_order', 	ci.`sort_order`,
+						'description', 	cid.`description`
+					)
+				) AS images
 			FROM " . DB_PREFIX . "category_to_store c2s
-			INNER JOIN " . DB_PREFIX . "category c
+			LEFT JOIN " . DB_PREFIX . "category c
 				ON c.`category_id` = c2s.`category_id`
-			INNER JOIN " . DB_PREFIX . "category_description cd
+			LEFT JOIN " . DB_PREFIX . "category_description cd
 				ON  cd.`category_id` 	= c2s.`category_id`
 				AND cd.`language_id` 	= '" . (int) $this->config->get('config_language_id') . "'
 				AND cd.`store_id` 		= c2s.`store_id`
+			LEFT JOIN " . DB_PREFIX . "category_image ci
+				ON ci.category_id = c2s.`category_id`
+				AND ci.store_id 	= c2s.`store_id`
+			LEFT JOIN " . DB_PREFIX . "category_image_description cid
+				ON cid.image_id 		= ci.image_id
+				AND cid.language_id = cd.language_id
+				AND cid.store_id 		= c2s.store_id
 			WHERE c2s.`category_id` = '" . (int) $category_id . "'
 				AND c2s.`store_id` 		= '" . (int) $this->config->get('config_store_id') . "'
 				AND c2s.`status` 			= 1
 			LIMIT 1
-		");
+		";
 
-		$data = $query->row;
-		$data['seo_keywords'] = json_decode($data['seo_keywords'], true);
-		$data['faq'] 					= json_decode($data['faq'], true);
-		$data['how_to'] 			= json_decode($data['how_to'], true);
-		$data['footer'] 			= json_decode($data['footer'], true);
+		$data = $this->db->query($sql)->row;
+		$data['seo_keywords'] = json_decode($data['seo_keywords'] ?? '[]', true);
+		$data['faq'] 					= json_decode($data['faq'] ?? '[]', true);
+		$data['how_to'] 			= json_decode($data['how_to'] ?? '[]', true);
+		$data['footer'] 			= json_decode($data['footer'] ?? '[]', true);
+		$data['images']				= json_decode($data['images'] ?? '[]', true);
+    usort(array: $data['images'], callback: fn ($a, $b) =>  $a['sort_order'] <=> $b['sort_order']);
 
 		$this->cache->set($categoryCacheName, $data);
 
-		return $query->row ?? [];
+		return $data;
 	}
 
 
@@ -109,72 +125,72 @@ class ModelCatalogCategory extends Model {
 	}
 
 	// Only used in upload\catalog\controller\extension\module\filter.php
-	public function getCategoryFilters($category_id) {
+	// public function getCategoryFilters($category_id) {
 
-		$sql = "
-			SELECT 
-				f.filter_id,
-				fd.name AS filter_name,
-				f.filter_group_id,
-				f.sort_order AS filter_sort,
-				fg2s.sort_order AS group_sort,
-				fgd.name AS group_name
-			FROM " . DB_PREFIX . "category_filter cf
+	// 	$sql = "
+	// 		SELECT 
+	// 			f.filter_id,
+	// 			fd.name AS filter_name,
+	// 			f.filter_group_id,
+	// 			f.sort_order AS filter_sort,
+	// 			fg2s.sort_order AS group_sort,
+	// 			fgd.name AS group_name
+	// 		FROM " . DB_PREFIX . "category_filter cf
 			
-			INNER JOIN " . DB_PREFIX . "filter f 
-				ON cf.filter_id = f.filter_id 
-				AND f.store_id = cf.store_id
+	// 		INNER JOIN " . DB_PREFIX . "filter f 
+	// 			ON cf.filter_id = f.filter_id 
+	// 			AND f.store_id = cf.store_id
 
-			INNER JOIN " . DB_PREFIX . "filter_group fg
-				ON fg.filter_group_id = f.filter_group_id
+	// 		INNER JOIN " . DB_PREFIX . "filter_group fg
+	// 			ON fg.filter_group_id = f.filter_group_id
 
-			INNER JOIN " . DB_PREFIX . "filter_description fd 
-				ON 	f.filter_id 		= fd.filter_id 
-				AND fd.language_id 	= '" . (int) $this->config->get('config_language_id') . "'
-				AND fd.store_id 		= cf.store_id 
+	// 		INNER JOIN " . DB_PREFIX . "filter_description fd 
+	// 			ON 	f.filter_id 		= fd.filter_id 
+	// 			AND fd.language_id 	= '" . (int) $this->config->get('config_language_id') . "'
+	// 			AND fd.store_id 		= cf.store_id 
 
-			INNER JOIN " . DB_PREFIX . "filter_group_to_store fg2s
-				ON f.filter_group_id 	= fg2s.filter_group_id 
-				AND fg2s.store_id 		= cf.store_id
+	// 		INNER JOIN " . DB_PREFIX . "filter_group_to_store fg2s
+	// 			ON f.filter_group_id 	= fg2s.filter_group_id 
+	// 			AND fg2s.store_id 		= cf.store_id
 
-			INNER JOIN " . DB_PREFIX . "filter_group_description fgd
-				ON  f.filter_group_id  	= fgd.filter_group_id
-				AND fgd.language_id 		= '" . (int) $this->config->get('config_language_id') . "'
-				AND fgd.store_id 				= cf.store_id
+	// 		INNER JOIN " . DB_PREFIX . "filter_group_description fgd
+	// 			ON  f.filter_group_id  	= fgd.filter_group_id
+	// 			AND fgd.language_id 		= '" . (int) $this->config->get('config_language_id') . "'
+	// 			AND fgd.store_id 				= cf.store_id
 
-			WHERE cf.category_id 	= '" . (int) $category_id . "'
-				AND cf.store_id 		= '" . (int) $this->config->get('config_store_id') . "'
+	// 		WHERE cf.category_id 	= '" . (int) $category_id . "'
+	// 			AND cf.store_id 		= '" . (int) $this->config->get('config_store_id') . "'
 
-			ORDER BY fg2s.sort_order, 
-				LCASE(group_name),
-				f.sort_order,
-				LCASE(filter_name)
-		";
+	// 		ORDER BY fg2s.sort_order, 
+	// 			LCASE(group_name),
+	// 			f.sort_order,
+	// 			LCASE(filter_name)
+	// 	";
 
-		$query = $this->db->query($sql);
+	// 	$query = $this->db->query($sql);
 
-		$filter_group_data = [];
+	// 	$filter_group_data = [];
 
-		foreach ($query->rows as $row) {
-			$group_id = $row['filter_group_id'];
+	// 	foreach ($query->rows as $row) {
+	// 		$group_id = $row['filter_group_id'];
 
-			if (!isset($filter_group_data[$group_id])) {
-				$filter_group_data[$group_id] = array(
-					'filter_group_id' => $group_id,
-					'name'            => $row['group_name'],
-					'filter'          => []
-				);
-			}
+	// 		if (!isset($filter_group_data[$group_id])) {
+	// 			$filter_group_data[$group_id] = array(
+	// 				'filter_group_id' => $group_id,
+	// 				'name'            => $row['group_name'],
+	// 				'filter'          => []
+	// 			);
+	// 		}
 
-			$filter_group_data[$group_id]['filter'][] = array(
-				'filter_id' => $row['filter_id'],
-				'name'      => $row['filter_name']
-			);
-		}
+	// 		$filter_group_data[$group_id]['filter'][] = array(
+	// 			'filter_id' => $row['filter_id'],
+	// 			'name'      => $row['filter_name']
+	// 		);
+	// 	}
 
-		// Reset keys to start from zero
-		return array_values($filter_group_data);
-	}
+	// 	// Reset keys to start from zero
+	// 	return array_values($filter_group_data);
+	// }
 
 
 	public function getCategoryLayoutId($category_id) {
