da7d6a95b1a39c66118b24cb876fb0a4fc3c4800
Tehnikus <tehnikus.ua@gmail.com>
2026-05-13 14:13:53 +0300

    Update translations

--- upload/catalog/language/en-gb/extension/module/facet_filter.php
+++ upload/catalog/language/en-gb/extension/module/facet_filter.php
@@ -13,9 +13,10 @@ $_['sort_trends']           = 'Trending';
 $_['sort_views']            = 'Views';
 $_['sort_date_added']       = 'Latest';
 $_['sort_available']        = 'Available for order';
-$_['sort_quantity']         = 'Quantity';
+$_['sort_featured']         = 'Featured first';
 $_['sort_trends_all_time']  = 'Popular all time';
 $_['sort_trends_by_date']   = 'Popular now';
+$_['sort_relevance']        = 'Best relevance';
 
 // Filter groups
 $_['group_category']      = 'Category';
@@ -28,6 +29,9 @@ $_['group_is_featured']   = 'Featured';
 $_['facet_is_available']  = 'Show only available for order';
 $_['facet_has_discount']  = 'Show only discounted';
 $_['facet_is_featured']   = 'Show only featured';
+$_['facet_latest']        = 'Latest';
+$_['facet_bestseller']    = 'Bestsellers';
+$_['facet_top_rated']     = 'Top rated';
 
 // Buttons
 $_['button_reset_filters'] = "Reset";
