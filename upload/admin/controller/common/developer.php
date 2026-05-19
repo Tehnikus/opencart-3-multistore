3fb8a2f7a6ace4774f7958df4efa08d015729926
Tehnikus <tehnikus.ua@gmail.com>
2026-05-20 01:11:59 +0300

    Admin: Refactor developer controller, remove unused and obsolete code

--- upload/admin/controller/common/developer.php
+++ upload/admin/controller/common/developer.php
@@ -4,24 +4,10 @@ class ControllerCommonDeveloper extends Controller {
 		$this->load->language('common/developer');
 
 		$data['user_token'] = $this->session->data['user_token'];
-
-		$data['developer_theme'] = $this->config->get('developer_theme');
-		$data['developer_sass'] = $this->config->get('developer_sass');
-
-		$eval = false;
-
-		$eval = '$eval = true;';
-
-		eval($eval);
-
-		if ($eval === true) {
-			$data['eval'] = true;
-		} else {
-			$this->load->model('setting/setting');
-
-			$this->model_setting_setting->editSetting('developer', array('developer_theme' => 1), 0);
-
-			$data['eval'] = false;
+		
+		$cacheSettings = ['developer_theme', 'developer_cache_categories', 'developer_cache_products', 'developer_cache_facet_pages', 'developer_cache_facet_list'];
+		foreach ($cacheSettings as $key) {
+			$data['cacheSettings'][$key] = (int) $this->config->get($key);
 		}
 
 		$this->response->setOutput($this->load->view('common/developer', $data));
@@ -37,7 +23,7 @@ class ControllerCommonDeveloper extends Controller {
 		} else {
 			$this->load->model('setting/setting');
 
-			$this->model_setting_setting->editSetting('developer', $this->request->post, 0);
+			$this->model_setting_setting->editSetting('developer', $this->request->post, (int) $this->session->data['store_id']);
 
 			$json['success'] = $this->language->get('text_success');
 		}
@@ -73,38 +59,6 @@ class ControllerCommonDeveloper extends Controller {
 			}
 
 			$json['success'] = sprintf($this->language->get('text_cache'), $this->language->get('text_theme'));
-		}
-
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
-	}
-
-	public function sass() {
-		$this->load->language('common/developer');
-
-		$json = array();
-
-		if (!$this->user->hasPermission('modify', 'common/developer')) {
-			$json['error'] = $this->language->get('error_permission');
-		} else {
-			// Before we delete we need to make sure there is a sass file to regenerate the css
-			$file = DIR_APPLICATION  . 'view/stylesheet/bootstrap.css';
-
-			if (is_file($file) && is_file(DIR_APPLICATION . 'view/stylesheet/sass/_bootstrap.scss')) {
-				unlink($file);
-			}
-			 
-			$files = glob(DIR_CATALOG  . 'view/theme/*/stylesheet/sass/_bootstrap.scss');
-			 
-			foreach ($files as $file) {
-				$file = substr($file, 0, -21) . '/bootstrap.css';
-
-				if (is_file($file)) {
-					unlink($file);
-				}
-			}
-
-			$json['success'] = sprintf($this->language->get('text_cache'), $this->language->get('text_sass'));
 		}
 
 		$this->response->addHeader('Content-Type: application/json');
