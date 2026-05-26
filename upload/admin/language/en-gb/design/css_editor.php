cc0861a86b0ac4358c1f5fea93226c155ab9737d
Tehnikus <tehnikus.ua@gmail.com>
2026-05-26 15:57:20 +0300

    Update translations

--- upload/admin/language/en-gb/design/css_editor.php
+++ upload/admin/language/en-gb/design/css_editor.php
@@ -2,4 +2,4 @@
 $_['heading_title'] = 'Edit CSS';
 $_['text_editor']   = 'Editor';
 $_['message_css_saved']        = 'The CSS file <b style="user-select: all">%s</b> saved successfully';
-$_['message_css_not_writable'] = 'The CSS file <b style="user-select: all">%s</b> is not writable. Seems like you need to set CHMOD 0755 or 0777 to your <b style="user-select: all">%s</b> directory.';
+$_['message_css_not_writable'] = 'The CSS file %s is not writable. Seems like you need to set CHMOD 0755 or 0777 to your <b style="user-select: all">%s</b> directory.';
