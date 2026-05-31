a02e7e7d69f50be1e166d5ddd26fb67d055caad2
Tehnikus <tehnikus.ua@gmail.com>
2026-05-31 18:21:37 +0300

    Frontend: Replace category 404 handling

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -26,31 +26,7 @@ class ControllerProductCategory extends Controller {
 			$this->response->setOutput($this->load->view('product/category', $data));
 
 		} else {
-			$url = '';
-
-			if (isset($this->request->get['path'])) {
-				$url .= '&path=' . $this->request->get['path'];
-			}
-
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('text_error'),
-				'href' => $this->url->link('product/category', $url)
-			);
-
-			$this->document->setTitle($this->language->get('text_error'));
-
-			$data['continue'] = $this->url->link('common/home');
-
-			$this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 404 Not Found');
-
-			$data['column_left'] = $this->load->controller('common/column_left');
-			$data['column_right'] = $this->load->controller('common/column_right');
-			$data['content_top'] = $this->load->controller('common/content_top');
-			$data['content_bottom'] = $this->load->controller('common/content_bottom');
-			$data['footer'] = $this->load->controller('common/footer');
-			$data['header'] = $this->load->controller('common/header');
-
-			$this->response->setOutput($this->load->view('error/not_found', $data));
+			$this->response->redirect($this->url->link('common/not_found', '', true));
 		}
 	}
 
