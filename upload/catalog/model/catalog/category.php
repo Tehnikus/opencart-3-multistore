69040d890e31e0f17d311390d0e397f0190a4dd4
Tehnikus <tehnikus.ua@gmail.com>
2026-05-31 18:25:49 +0300

    SQL: Comment out unused CTE, this data is now retrieved by product facet engine

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -16,18 +16,18 @@ class ModelCatalogCategory extends Model {
 		}
 
 		$sql = "
-			WITH category_stats AS (
-				SELECT
-					`category_id`,
-					MIN(`current_price`) 	AS `price_min`,
-					MAX(`current_price`) 	AS `price_max`,
-					AVG(`rating_avg`) 		AS `rating`,
-					SUM(`review_count`)		AS `reviews`,
-					COUNT(`product_id`)		AS `product_count`
-				FROM " . DB_PREFIX . "facet_sort
-				WHERE `category_id` = {$category_id}
-					AND `store_id` 		= {$store_id}
-			)
+			-- WITH category_stats AS (
+			-- 	SELECT
+			-- 		`category_id`,
+			-- 		MIN(`current_price`) 	AS `price_min`,
+			-- 		MAX(`current_price`) 	AS `price_max`,
+			-- 		AVG(`rating_avg`) 		AS `rating`,
+			-- 		SUM(`review_count`)		AS `reviews`,
+			-- 		COUNT(`product_id`)		AS `product_count`
+			-- 	FROM " . DB_PREFIX . "facet_sort
+			-- 	WHERE `category_id` = {$category_id}
+			-- 		AND `store_id` 		= {$store_id}
+			-- )
 			SELECT 
 				c.`category_id`,
 				c2s.`store_id`,
@@ -49,11 +49,11 @@ class ModelCatalogCategory extends Model {
 				cd.`footer`,
 				cd.`date_modified`,
 				cd.`language_id`,
-				cs.`price_min`,
-				cs.`price_max`,
-				cs.`rating`,
-				cs.`reviews`,
-				cs.`product_count`,
+				-- cs.`price_min`,
+				-- cs.`price_max`,
+				-- cs.`rating`,
+				-- cs.`reviews`,
+				-- cs.`product_count`,
 				JSON_ARRAYAGG(
 					JSON_OBJECT(
 						'image', 				ci.`image`,
@@ -93,8 +93,8 @@ class ModelCatalogCategory extends Model {
 				ON cid.image_id 		= ci.image_id
 				AND cid.language_id = cd.language_id
 				AND cid.store_id 		= c2s.store_id
-			LEFT JOIN category_stats cs
-				ON cs.`category_id` = c2s.`category_id`
+			-- LEFT JOIN category_stats cs
+			-- 	ON cs.`category_id` = c2s.`category_id`
 			WHERE c2s.`category_id` = '" . (int) $category_id . "'
 				AND c2s.`store_id` 		= '" . (int) $this->config->get('config_store_id') . "'
 				AND c2s.`status` 			= 1
